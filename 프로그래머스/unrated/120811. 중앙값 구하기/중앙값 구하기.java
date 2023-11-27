cf726de38f5650c7215f6b5c2908066adb867fe4
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-27 22:09:53 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return array[(int) array.length / 2];
+    }
+}
