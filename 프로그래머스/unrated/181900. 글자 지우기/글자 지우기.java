796681b4f7ec3ce85a57cd67e4140c16a83d09f1
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 22:58:46 +0900

    [level unrated] Title: 글자 지우기, Time: 0.45 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        String[] myChars = my_string.split("");
+        StringBuilder sb = new StringBuilder();
+        for (int i : indices) {
+            myChars[i] = "";
+        }
+        for (String str : myChars) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
