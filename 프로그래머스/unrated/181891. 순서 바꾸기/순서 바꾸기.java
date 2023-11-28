6a8bda3e8404c3b4ca4c8c88f78f40e8b8ca841e
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 22:57:49 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        System.arraycopy(num_list, 0, answer, num_list.length - n, n);
+        System.arraycopy(num_list, n, answer, 0, num_list.length - n);
+        return answer;
+    }
+}
