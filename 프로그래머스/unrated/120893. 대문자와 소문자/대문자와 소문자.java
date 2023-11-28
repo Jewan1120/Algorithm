0902c39a3734d60b9cebf62e27002556a12a49ef
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 22:40:55 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.79 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            if(Character.isLowerCase(c)){
+                sb.append(Character.toUpperCase(c));
+            }else{
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        return sb.toString();
+    }
+}
