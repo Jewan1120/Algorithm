9efb158cbd1145c2843b4075037dcfeae3dbd3a4
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 22:45:15 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.04 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i][i] = 1;
+        }
+        return answer;
+    }
+}
