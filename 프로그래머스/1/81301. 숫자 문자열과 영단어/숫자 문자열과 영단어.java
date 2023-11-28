7536cb4b009b7618317cadf1b679fa90148b26a6
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 23:14:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String s) {
+        s = s.toLowerCase();
+        String[][] alphabets = {{"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"},
+                {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}};
+        for (String[] code : alphabets) {
+            s = s.replace(code[0], code[1]);
+        }
+        return Integer.parseInt(s);
+    }
+}
