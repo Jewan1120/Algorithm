d342c675899914f106f449635f8ed2878259e2c8
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-11-28 23:06:22 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        System.arraycopy(num_list, 0, answer, 0, num_list.length);
+        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+              answer[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        } else {
+              answer[num_list.length] = num_list[num_list.length - 1] * 2;
+        }
+        return answer;
+    }
+}
