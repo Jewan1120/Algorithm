899036af26d5cf0f5af90f7b36a5a5e6b646446b
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2023-12-17 19:43:59 +0900

    [level 2] Title: 가장 큰 수, Time: 2.42 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strArr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strArr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strArr, (o1, o2) -> Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2));
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArr) {
+            sb.append(str);
+        }
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
+    }
+}
