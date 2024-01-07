d585c58607a614258e5c22e70284bfdfe491251f
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-07 22:49:35 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.37 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        int gcdA = arrayA[0];
+        int gcdB = arrayB[0];
+        for (int i = 1; i < arrayA.length; i++) {
+            int xA = arrayA[i];
+            int xB = arrayB[i];
+            while (xA != 0) {
+                int tmp = gcdA % xA;
+                gcdA = xA;
+                xA = tmp;
+            }
+            while (xB != 0) {
+                int tmp = gcdB % xB;
+                gcdB = xB;
+                xB = tmp;
+            }
+        }
+        boolean chkA = false;
+        boolean chkB = false;
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] % gcdB == 0) {
+                chkB = true;
+            }
+            if (arrayB[i] % gcdA == 0) {
+                chkA = true;
+            }
+        }
+        if (gcdA != 1 && !chkA) {
+            answer = Math.max(answer, gcdA);
+        }
+        if (gcdB != 1 && !chkB) {
+            answer = Math.max(answer, gcdB);
+        }
+        return answer;
+    }
+}
