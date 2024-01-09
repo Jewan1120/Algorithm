85096ff212d99e8c31545374ae89453fc841541e
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-10 00:33:32 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.71 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        Arrays.sort(data, (o1, o2) -> o1[col - 1] != o2[col - 1] ? o1[col - 1] - o2[col - 1] : o2[0] - o1[0]);
+        for (int i = row_begin; i <= row_end; i++) {
+            int s = 0;
+            for (int j = 0; j < data[0].length; j++) {
+                s += data[i - 1][j] % i;
+            }
+            answer ^= s;
+        }
+        return answer;
+    }
+}
