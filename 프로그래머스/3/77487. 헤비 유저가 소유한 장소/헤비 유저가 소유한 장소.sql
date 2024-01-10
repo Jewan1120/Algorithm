c8b0441d098dd95dd4eb7c36f7021b8bf75978b9
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-10 17:45:17 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,15 @@
+SELECT
+    *
+FROM
+    PLACES
+WHERE
+    HOST_ID IN(
+        SELECT
+            HOST_ID
+        FROM
+            PLACES
+        GROUP BY
+            HOST_ID
+        HAVING
+            COUNT(HOST_ID) >= 2
+    )
