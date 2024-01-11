a392f24cd7ebe7fcb018428fc5e55d214e0d39e9
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-11 16:38:42 +0900

    [level Lv.3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,15 @@
+SELECT
+    CONCAT('/home/grep/src/', A.BOARD_ID , '/', FILE_ID, FILE_NAME, FILE_EXT) FILE_PATH
+FROM
+    USED_GOODS_BOARD A
+    INNER JOIN USED_GOODS_FILE B
+        ON A.BOARD_ID = B.BOARD_ID
+WHERE
+    VIEWS IN (
+        SELECT
+            MAX(VIEWS)
+        FROM
+            USED_GOODS_BOARD
+    )
+ORDER BY
+    FILE_ID DESC
