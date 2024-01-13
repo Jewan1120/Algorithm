ec1adb28f0b18cd61257afa52a81b962a6571260
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-13 16:25:31 +0900

    [level Lv.4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,15 @@
+
+SET @hour := -1;
+SELECT (@hour := @hour + 1) as HOUR,
+    (
+        SELECT 
+            COUNT(*) 
+        FROM 
+            ANIMAL_OUTS 
+        WHERE 
+            HOUR(DATETIME) = @hour
+    ) as COUNT
+FROM
+    ANIMAL_OUTS
+WHERE 
+    @hour < 23
