8e43067bcd1297d0f555d272efefd022a3546a39
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-01-30 23:44:46 +0900

    [level 3] Title: 야근 지수, Time: 127.34 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,20 +1,42 @@
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 class Solution {
     public long solution(int n, int[] works) {
-        long answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int w : works) {
-            pq.add(w);
-        }
-        while (n > 0) {
-            if (pq.peek() == 0) {
-                break;
+        long answer = Long.MAX_VALUE;
+        Queue<int[]> wQue = new ArrayDeque<>();
+        Queue<boolean[]> chkQue = new ArrayDeque<>();
+        Queue<Integer> nQue = new ArrayDeque<>();
+        wQue.add(works);
+        chkQue.add(new boolean[works.length]);
+        nQue.add(n);
+        while (!wQue.isEmpty()) {
+            int[] wArr = wQue.poll();
+            boolean[] chkArr = chkQue.poll();
+            int tmpN = nQue.poll();
+            if (tmpN == 0) {
+                long result = 0;
+                for (int i : wArr) {
+                    result += Math.pow(i, 2);
+                }
+                answer = Math.min(answer, result);
+                continue;
             }
-            pq.add(pq.poll() - 1);
-            n--;
+            for (int i = 0; i < chkArr.length; i++) {
+                if (!chkArr[i]) {
+                    chkArr[i] = true;
+                    for (int j = 1; j <= wArr[i]; j++) {
+                        int[] tmpW = wArr.clone();
+                        tmpW[i] -= j;
+                        wQue.add(tmpW);
+                        chkQue.add(chkArr.clone());
+                        nQue.add(tmpN - j);
+                    }
+                    chkArr[i] = false;
+                }
+            }
         }
-        for (Integer i : pq) {
-            answer += Math.pow(i, 2);
+        if (answer == Long.MAX_VALUE) {
+            answer = 0;
         }
         return answer;
     }
