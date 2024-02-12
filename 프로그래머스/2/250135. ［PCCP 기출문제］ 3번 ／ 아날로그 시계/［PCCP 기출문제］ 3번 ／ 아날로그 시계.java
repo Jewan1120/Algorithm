2036008bcc02bdb95914059e75731073fdf101a7
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-02-12 16:34:00 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 19.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -3,21 +3,62 @@ class Solution {
         int answer = 0;
         int now = h1 * 3600 + m1 * 60 + s1;
         int end = h2 * 3600 + m2 * 60 + s2;
-        int h = h1, m = m1, s = s1;
-        float ihA = 1f / 120, imA = 1f / 10, isA = 6;
-        float hA = (now * ihA) % 360, mA = (now * imA) % 360, sA = (now * isA) % 360;
+        if (now == 0 || now == 43200) {
+            answer++;
+        }
         while (now < end) {
-            if (sA < hA && hA + ihA <= sA + isA) {
+            double[] nowAngle = stoA(now);
+            double[] nextAngle = stoA(now + 1);
+            boolean hourMatch = hourChk(nowAngle, nextAngle);
+            boolean minMatch = minChk(nowAngle, nextAngle);
+            if (hourMatch) {
                 answer++;
             }
-            if (sA < mA && mA + imA <= sA + isA) {
+            if (minMatch) {
                 answer++;
+            }
+            if (hourMatch && minMatch) {
+                if (Double.compare(nextAngle[0], nextAngle[1]) == 0) {
+                    answer--;
+                }
             }
             now++;
-            hA = (hA + ihA) % 360;
-            mA = (mA + imA) % 360;
-            sA = (sA + isA) % 360;
         }
         return answer;
+    }
+    
+    public double[] stoA(int sec) {
+        double[] angles = new double[3];
+        int h = sec / 3600;
+        int m = sec % 3600 / 60;
+        int s = sec % 3600 % 60;
+        angles[0] = (h % 12) * 30d + m * 1 / 2d + s * 1 / 120d;
+        angles[1] = m * 6d + s * 1 / 10d;
+        angles[2] = s * 6d;
+        return angles;
+    }
+    
+    public boolean hourChk(double[] now, double[] next) {
+        double hA = now[0], sA = now[2];
+        double nhA = next[0], nsA = next[2];
+        if (Double.compare(hA, sA) > 0 && Double.compare(nhA, nsA) <= 0) {
+            return true;
+        }
+        if (Double.compare(sA, 354d) == 0 && Double.compare(hA, 354d) > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean minChk(double[] now, double[] next) {
+        double mA = now[1], sA = now[2];
+        double nmA = next[1], nsA = next[2];
+        if (Double.compare(mA, sA) > 0 && Double.compare(nmA, nsA) <= 0) {
+            return true;
+        }
+        if (Double.compare(sA, 354d) == 0 && Double.compare(mA, 354d) > 0) {
+            return true;
+        }
+        return false;
     }
 }
