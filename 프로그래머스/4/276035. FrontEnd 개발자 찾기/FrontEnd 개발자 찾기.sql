36ab558be3f69db04b9ba988085bd2abd37dcc51
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-02-26 14:01:44 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,20 +1,18 @@
-WITH FE AS (
-    SELECT
-        *
-    FROM
-        SKILLCODES
-    WHERE
-        CATEGORY = 'Front End'
-)
-
 SELECT
-    DISTINCT ID
+    ID
     , EMAIL
     , FIRST_NAME
     , LAST_NAME
 FROM
-    DEVELOPERS A
-    INNER JOIN FE B
-        ON A.SKILL_CODE & B.CODE
+    DEVELOPERS 
+WHERE
+    SKILL_CODE & (
+        SELECT
+            SUM(CODE)
+        FROM
+            SKILLCODES
+        WHERE
+            CATEGORY = 'Front End'
+    )
 ORDER BY
     ID
