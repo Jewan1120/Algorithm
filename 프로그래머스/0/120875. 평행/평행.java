292a5df3b87aa490e02fe06ff72d662fea2d0e09
Jewan1120 <hawaii1468@gmail.com>
2024-03-02 22:05:01 +0900

    레벨 정리

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        int dot1 = 0;
-        int dot2 = 0;
         int[][] points = {{0, 1, 2, 3}, {0, 2, 1, 3}, {0, 3, 1, 2}};
         for (int[] point : points) {
             double d1 = ((double) (dots[point[0]][1] - dots[point[1]][1])
