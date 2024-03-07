d1f36511551b63c09b3203759e1901e28f1effea
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-03-07 23:59:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.81 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+        for (int i = 0; i < costs.length; i++) {
+            int rootX = find(parent, costs[i][0]);
+            int rootY = find(parent, costs[i][1]);
+            int weight = costs[i][2];
+            if (rootX != rootY) {
+                union(parent, rootX, rootY);
+                answer += weight;
+            }
+        }
+        return answer;
+    }
+    
+    public int find(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent, parent[node]);
+        }
+        return parent[node];
+    }
+
+    public void union(int[] parent, int rootX, int rootY) {
+        parent[rootX] = rootY;
+    }
+}
