f32aa2c6faace8c244ce01c4eeac45793353a903
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-03-08 23:50:14 +0900

    [level 3] Title: 섬 연결하기, Time: 0.91 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,28 +1,57 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 class Solution {
+    
+    class Edge implements Comparable<Edge> {
+        int to;
+        int weight;
+        
+        public Edge(int to, int weight){
+            this.to = to;
+            this.weight = weight;
+        }
+        
+        @Override
+        public int compareTo(Edge other){
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
+    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
         }
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        for (int i = 0; i < costs.length; i++) {
-            int rootX = find(parent, costs[i][0]);
-            int rootY = find(parent, costs[i][1]);
+        for(int i = 0; i < costs.length; i++){
+            int x = costs[i][0];
+            int y = costs[i][1];
             int weight = costs[i][2];
-            if (rootX != rootY) {
-                parent[rootX] = rootY;
-                answer += weight;
+            graph.get(x).add(new Edge(y, weight));
+            graph.get(y).add(new Edge(x, weight));
+        }
+        boolean[] inMST = new boolean[n];
+        PriorityQueue<Edge> que = new PriorityQueue<>();
+        que.add(new Edge(0, 0));
+        while(!que.isEmpty()){
+            Edge cur = que.poll();
+            int u = cur.to;
+            if(inMST[u]) continue;
+            
+            inMST[u] = true;
+            answer += cur.weight;
+            
+            for(Edge neighbor : graph.get(u)){
+                int v = neighbor.to;
+                int weight = neighbor.weight;
+                if (!inMST[v]) {
+                    que.add(new Edge(v, weight));
+                }
             }
         }
         return answer;
-    }
-    
-    public int find(int[] parent, int node) {
-        if (parent[node] != node) {
-            parent[node] = find(parent, parent[node]);
-        }
-        return parent[node];
-    }
+    }   
 }
