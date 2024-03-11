14c3e8d029172db1168fb8d1a723188f0d262b84
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-03-11 10:42:06 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,25 @@
+WITH HIGHEST AS (
+    SELECT
+        EMP_NO
+        , SUM(SCORE) SCORE
+    FROM
+        HR_GRADE
+    WHERE
+        YEAR = '2022'
+    GROUP BY
+        EMP_NO
+    ORDER BY
+        SUM(SCORE) DESC
+    LIMIT 1
+)
+
+SELECT
+    B.SCORE
+    , A.EMP_NO
+    , A.EMP_NAME
+    , A.POSITION
+    , A.EMAIL
+FROM
+    HR_EMPLOYEES A
+    INNER JOIN HIGHEST B
+        ON A.EMP_NO = B.EMP_NO
