3c18a9ad5d04feaf97729f69bccfffaf590a64e7
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-03-13 23:32:41 +0900

    [level 3] Title: 여행경로, Time: 0.68 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,29 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
 import java.util.TreeMap;
 class Solution {
     public String[] solution(String[][] tickets) {
         int len = tickets.length + 1;
         String[] answer = new String[len];
-
-        TreeMap<String, TreeMap<String, Integer>> ticketMap = new TreeMap<>();
-
-        for (String[] ticket : tickets) {
-            String a = ticket[0];
-            String b = ticket[1];
-            if (!ticketMap.containsKey(a)) {
-                ticketMap.put(a, new TreeMap<>((o1, o2) -> o1.compareTo(o2)));
-            }
-            TreeMap<String, Integer> fromMap = ticketMap.get(a);
-            if (!fromMap.containsKey(b)) {
-                fromMap.put(b, 0);
-            }
-            fromMap.replace(b, fromMap.get(b) + 1);
-        }
+        Map<String, TreeMap<String, Integer>> ticketMap = buildTicketMap(tickets);
         answer[0] = "ICN";
         dfs(ticketMap, answer, "ICN", 1);
         return answer;
     }
 
-    public boolean dfs(TreeMap<String, TreeMap<String, Integer>> ticketMap, String[] answer, String cur,
+    private Map<String, TreeMap<String, Integer>> buildTicketMap(String[][] tickets) {
+        Map<String, TreeMap<String, Integer>> ticketMap = new HashMap<>();
+        for (String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+            ticketMap.putIfAbsent(from, new TreeMap<>());
+            TreeMap<String, Integer> fromMap = ticketMap.get(from);
+            fromMap.put(to, fromMap.getOrDefault(to, 0) + 1);
+        }
+        return ticketMap;
+    }
+
+    public boolean dfs(Map<String, TreeMap<String, Integer>> ticketMap, String[] answer, String cur,
             int depth) {
         if (depth == answer.length) {
             return true;
