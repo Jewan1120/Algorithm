39adb9405061be7ff738ad8b88fe13defe068caf
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-12 17:05:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        bw.write(String.valueOf(a + b + c));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
