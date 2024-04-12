f134966ae1ad490b3918c223a3ad8bb965006d65
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-13 02:05:38 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long sum = Math.abs(a - b);
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
