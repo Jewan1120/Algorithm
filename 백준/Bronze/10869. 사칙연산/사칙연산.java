5a9f42dc78c1ffa2331642f74568684d97fa8150
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-12 16:57:27 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(a + b));
+        bw.newLine();
+        bw.write(String.valueOf(a - b));
+        bw.newLine();
+        bw.write(String.valueOf(a * b));
+        bw.newLine();
+        bw.write(String.valueOf(a / b));
+        bw.newLine();
+        bw.write(String.valueOf(a % b));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
