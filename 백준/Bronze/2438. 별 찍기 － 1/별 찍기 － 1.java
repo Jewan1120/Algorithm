90276edaafc683f11182c9cafd45d06897b357c5
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-14 17:17:43 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            bw.write("*".repeat(i));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
