ceb28a485fc689d02476d698412c049e42726739
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-15 00:18:44 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw.write(String.valueOf(br.readLine().length()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
