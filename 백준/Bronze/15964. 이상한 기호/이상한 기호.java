2166991ffbf79f25bdd18cc283b3f9cefc8a3dd8
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-14 23:43:22 +0900

    [Bronze V] Title: 이상한 기호, Time: 128 ms, Memory: 14212 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf((N * N) - (M * M)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
