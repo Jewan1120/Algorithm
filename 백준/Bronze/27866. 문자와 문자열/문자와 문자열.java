ea3707b99439944df5e5dc23a09861cea4490269
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-15 00:36:42 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        bw.write(String.valueOf(str.charAt(Integer.parseInt(br.readLine()) - 1)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
