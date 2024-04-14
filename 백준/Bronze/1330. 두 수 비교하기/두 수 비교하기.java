4d21e03a1d93e20fd3c8ec889d02c26e3b3bb932
백제완(ベク) <116615068+Jewan1120@users.noreply.github.com>
2024-04-14 16:46:52 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,18 +1,22 @@
 import java.io.IOException;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        String result = a == b ? "==" : a > b ? ">" : "<";
+        String result = "";
+        if (a < b) result = "<"; 
+            else if (a > b) result = ">";
+            else result = "==";
         bw.write(result);
         bw.flush();
         bw.close();
