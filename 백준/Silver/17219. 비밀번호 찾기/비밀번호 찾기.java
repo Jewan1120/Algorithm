463996a3e84ed3e37ed23fbc2ccdc8bfe5214a84
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-04-21 21:22:26 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 592 ms, Memory: 52544 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -15,8 +17,13 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             hm.put(st.nextToken(), st.nextToken());
         }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (; M > 0; M--) {
-            System.out.println(hm.get(br.readLine()));
+            bw.write(hm.get(br.readLine()));
+            bw.newLine();
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
