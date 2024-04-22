f5fc4d135aeecfc65ce17a9aca26957f3b4af8e7
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-04-22 18:06:12 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,30 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<String> hs;
         HashMap<String, Integer> hm;
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
         for (; n > 0; n--) {
             int m = Integer.parseInt(br.readLine());
-            hs = new HashSet<>();
             hm = new HashMap<>();
             for (; m > 0; m--) {
                 st = new StringTokenizer(br.readLine());
                 st.nextToken();
                 String cate = st.nextToken();
-                hs.add(cate);
                 hm.putIfAbsent(cate, 1);
                 hm.replace(cate, hm.get(cate) + 1);
             }
             int cnt = 1;
-            for (String cate : hs) {
+            for (String cate : hm.keySet()) {
                 cnt *= hm.get(cate);
             }
             System.out.println(cnt - 1);
