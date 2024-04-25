5184097c4609c6b358f56eaa4589912027630e31
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-04-25 09:57:20 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 44288 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+        Arrays.sort(arr, (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        int cnt = 0;
+        int e = 0;
+        for (int i = 0; i < n; i++) {
+            int s = arr[i][0];
+            if (e <= s) {
+                cnt++;
+                e = arr[i][1];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
