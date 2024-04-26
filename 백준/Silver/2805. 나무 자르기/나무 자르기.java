6f62eec8e3c02be69774d7861f87ea30901375c2
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-04-26 16:53:30 +0900

    [Silver II] Title: 나무 자르기, Time: 960 ms, Memory: 120524 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int l = 0, r = arr[n - 1];
+        while (l < r) {
+            int mid = (l + r) / 2;
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > mid) {
+                    sum += arr[i] - mid;
+                }
+            }
+            if (sum < m) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        System.out.println(l - 1);
+        br.close();
+    }
+}
