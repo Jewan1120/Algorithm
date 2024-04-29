ff7d5462ce9e9a68f9a67e8388149cd08fecf1b8
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-04-29 19:05:57 +0900

    [Silver I] Title: 카잉 달력, Time: 344 ms, Memory: 24976 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int r = -1;
+            for (int j = x; j < M * N; j += M) {
+                if (j % N == y) {
+                    r = j + 1;
+                    break;
+                }
+            }
+            bw.write(r + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
