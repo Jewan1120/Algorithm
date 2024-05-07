64c83b08adf6348bc84b9cff17e708a7927662f0
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-05-07 18:00:17 +0900

    [Silver II] Title: N과 M (10), Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] nums;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        nums = new int[n];
+        visited = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int p, int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int temp = 0;
+        for (int i = p; i < n; i++) {
+            if (nums[i] != temp && !visited[i]) {
+                visited[i] = true;
+                temp = nums[i];
+                arr[depth] = nums[i];
+                dfs(i, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
