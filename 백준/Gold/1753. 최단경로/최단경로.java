2de83c6f33f8feefde1ec87d2ec0052071d362bc
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-05-13 01:39:06 +0900

    [Gold IV] Title: 최단경로, Time: 760 ms, Memory: 109508 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -27,8 +26,11 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(br.readLine());
         List<List<Node>> graph = new ArrayList<>();
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
+            dist[i] = Integer.MAX_VALUE;
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -37,21 +39,21 @@ public class Main {
             int w = Integer.parseInt(st.nextToken());
             graph.get(v).add(new Node(u, w));
         }
-        boolean[] visited = new boolean[n + 1];
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[s] = 0;
-        visited[s] = true;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((v1, v2) -> dist[v1] - dist[v2]);
-        pq.offer(s);
+        PriorityQueue<Node> pq = new PriorityQueue<>((v1, v2) -> v1.weight - v2.weight);
+        pq.offer(new Node(s, 0));
         while (!pq.isEmpty()) {
-            int v = pq.poll();
+            Node cur = pq.poll();
+            int v = cur.to;
+            int w = cur.weight;
+            if (visited[v]) {
+                continue;
+            }
+            visited[v] = true;
             for (Node node : graph.get(v)) {
-                int u = node.to;
-                int w = node.weight;
-                if (dist[u] > dist[v] + w) {
-                    dist[u] = dist[v] + w;
-                    pq.offer(u);
+                if (dist[node.to] > node.weight + w) {
+                    dist[node.to] = node.weight + w;
+                    pq.offer(new Node(node.to, node.weight + w));
                 }
             }
         }
