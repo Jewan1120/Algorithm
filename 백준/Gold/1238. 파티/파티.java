ec86d89968d566b70112fcea2f0bc66ec838be1c
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-05-22 13:40:00 +0900

    [Gold III] Title: 파티, Time: 144 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -15,47 +15,48 @@ public class Main {
     static int n;
     static int m;
     static int x;
-    static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
-    static int[][] spendTime;
+    static ArrayList<Edge>[] to, from;
     
     public static void main(String[] args) throws Exception {
         n = read();
         m = read();
         x = read();
-        spendTime = new int[n + 1][];
+        to = new ArrayList[n + 1];
+        from = new ArrayList[n + 1];
         int max = 0;
         for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+            to[i] = new ArrayList<>();
+            from[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             int u = read();
             int v = read();
             int w = read();
-            graph.get(u).add(new Edge(v, w));
+            to[u].add(new Edge(v, w));
+            from[v].add(new Edge(u, w));
         }
+        int[] distTo = find(to, x);
+        int[] distFrom = find(from, x);
         for (int i = 1; i <= n; i++) {
-            spendTime[i] = find(i);
-        }
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, spendTime[i][x] + spendTime[x][i]);
+            max = Math.max(max, distTo[i] + distFrom[i]);
         }
         System.out.println(max);
     }
     
-    private static int[] find(int s) {
+    private static int[] find(ArrayList<Edge>[] graph, int s) {
         int[] dist = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             dist[i] = Integer.MAX_VALUE;
         }
         dist[s] = 0;
         boolean[] visited = new boolean[n + 1];
-        PriorityQueue<Integer> pq = new PriorityQueue<>((v1, v2) -> dist[v1] - dist[v2]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((v1, v2) -> Integer.compare(dist[v1], dist[v2]));
         pq.offer(s);
         while (!pq.isEmpty()) {
             int u = pq.poll();
             if (visited[u]) continue;
             visited[u] = true;
-            for (Edge edge : graph.get(u)) {
+            for (Edge edge : graph[u]) {
                 int v = edge.t;
                 int w = edge.w;
                 if (!visited[v] && dist[u] + w < dist[v]) {
