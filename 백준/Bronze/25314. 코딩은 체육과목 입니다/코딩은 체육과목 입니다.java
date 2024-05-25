2198507ea65ee905bc87762d8890c7a89635126e
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-05-25 21:13:19 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read() / 4;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++){
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
