a18ebf3575ae09d2c222d4ef45df300d2d50e741
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-05-28 21:17:32 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,18 +1,12 @@
 public class Main {
-
     public static void main(String[] args) throws Exception {
-        int x = read();
-        int y = read();
-        int w = read();
-        int h = read();
+        int x = read(), y = read(), w = read(), h = read();
         System.out.println(Math.min(x, Math.min(y, Math.min(w - x, h - y))));
     }
-
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
+        while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
-        }
         return n;
     }
 }
