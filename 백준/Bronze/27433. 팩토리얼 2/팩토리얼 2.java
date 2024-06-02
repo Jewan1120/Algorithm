8f089d5ea840a4d49193406b4fc2a4005bc19d9e
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-06-02 23:34:31 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        long fact = 1;
+        for (int i = 2; i <= n; i++)
+            fact *= i;
+        System.out.println(fact);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
