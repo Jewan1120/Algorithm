b81f7687a3fc478ffcae81beae6ecd93f5cb8d88
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-06-19 20:10:04 +0900

    [Silver V] Title: 칠무해, Time: 1276 ms, Memory: 300144 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.java
+++ 백준/Silver/14729. 칠무해/칠무해.java
@@ -7,14 +7,24 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        double[] score = new double[n];
-        for(int i = 0; i <n ;i++) {
-            score[i] = Double.parseDouble(br.readLine());
+        double[] bs = new double[8];
+        Arrays.fill(bs, 100);
+        for (int i = 0; i < n; i++) {
+            double num = Double.parseDouble(br.readLine());
+            int idx = Arrays.binarySearch(bs, 0, 7, num);
+            if (idx < 0) {
+                idx = -(idx + 1);
+            }
+            if (idx < 7) {
+                for (int j = 6; j > idx; j--) {
+                    bs[j] = bs[j - 1];
+                }
+            }
+            bs[idx] = num;
         }
-        Arrays.sort(score);
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < 7; i ++) {
-            sb.append(String.format("%.3f", score[i])).append("\n");
+        for (int i = 0; i < 7; i++) {
+            sb.append(String.format("%.3f", bs[i])).append("\n");
         }
         System.out.println(sb);
     }
