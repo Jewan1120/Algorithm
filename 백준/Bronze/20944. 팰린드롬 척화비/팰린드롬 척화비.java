6e3afa3b53e898d36f1a69154212a9449c0e7fe4
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-06-26 17:48:46 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 196 ms, Memory: 16040 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   
+        int t = Integer.parseInt(br.readLine());
+        System.out.println("a".repeat(t));
+    }
+}
