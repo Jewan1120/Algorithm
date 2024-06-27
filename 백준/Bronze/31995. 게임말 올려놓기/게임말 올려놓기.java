a6dd5dd46272d640ced645e297c3e3c800aa476a
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-06-27 14:40:06 +0900

    [Bronze II] Title: 게임말 올려놓기, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/31995. 게임말 올려놓기/게임말 올려놓기.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int n = read(), m = read();
+        System.out.println(((n - 2) * 2 + 2) * (m - 1));
+    }
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
