907a1a66afa7139ccee18a046c111814aaa1303f
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-05 22:50:14 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = (read() * read()) - read();
+        System.out.println(n > 0 ? n : 0);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
