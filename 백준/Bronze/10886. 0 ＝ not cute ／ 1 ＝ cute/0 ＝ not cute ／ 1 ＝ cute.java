3e0cf5004998e97770c80a2def23d3e87d93bf5d
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-11 23:28:10 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 100 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int cnt = 0;
+        while (n-- > 0)
+            if (read() == 1)
+                cnt++;
+            else
+                cnt--;
+        System.out.println(cnt > 0 ? "Junhee is cute!" : "Junhee is not cute!");
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
