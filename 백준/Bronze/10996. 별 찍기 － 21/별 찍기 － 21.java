b7c08551af7b732e3656188f2b64dac74ef1de5c
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-19 02:09:25 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append("* ".repeat((n + 1) / 2)).append("\n");
+            sb.append(" *".repeat(n / 2)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
