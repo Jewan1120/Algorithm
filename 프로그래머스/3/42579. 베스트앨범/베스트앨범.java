ca7cf09cd6808d6d81df009690b119a62e213a0f
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-26 13:29:42 +0900

    [level 3] Title: 베스트앨범, Time: 4.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,41 +3,26 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        HashMap<String, PriorityQueue<int[]>> rankHm = new HashMap<>();
-        HashMap<String, Integer> totalHm = new HashMap<>();
-        ArrayList<Object[]> totalAL = new ArrayList<>();
-        HashMap<String, Integer> genreHs = new HashMap<>();
-        for (int i = 0; i < plays.length; i++) {
-            String genre = genres[i];
-            int play = plays[i];
-            if (!rankHm.containsKey(genre)) {
-                PriorityQueue<int[]> tmpQue = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
-                int size = genreHs.size();
-                rankHm.put(genre, tmpQue);
-                genreHs.put(genre, size);
-                totalAL.add(new Object[] {genre, 0});
-            }
-            int idx = genreHs.get(genre);
-            rankHm.get(genre).add(new int[] {i, play});
-            totalHm.put(genre, totalHm.getOrDefault(genre, 0) + play);
-            totalAL.get(idx)[1] = (int) totalAL.get(idx)[1] + play;
+        ArrayList<Integer> album = new ArrayList<>();
+        HashMap<String, Integer> genresRank = new HashMap<>();
+        HashMap<String, PriorityQueue<int[]>> playsRank = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            genresRank.putIfAbsent(genres[i], 0);
+            genresRank.replace(genres[i], genresRank.get(genres[i]) + plays[i]);
+            playsRank.putIfAbsent(genres[i], new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]));
+            playsRank.get(genres[i]).add(new int[] {i, plays[i]});
         }
-        totalAL.sort((o1, o2) -> (int) o2[1] - (int) o1[1]);
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < rankHm.size(); i++) {
-            String genre = (String) totalAL.get(i)[0];
-            PriorityQueue<int[]> tmpQue = rankHm.get(genre);
-            int queSize = tmpQue.size();
-            result.add(tmpQue.poll()[0]);
-            if (queSize > 1) {
-                result.add(tmpQue.poll()[0]);
+        ArrayList<String> keys = new ArrayList<>(genresRank.keySet());
+        keys.sort((o1, o2) -> genresRank.get(o2) - genresRank.get(o1));
+        for (String genre : keys) {
+            int len = Math.min(2, playsRank.get(genre).size());
+            for (int i = 0; i < len; i++) {
+                album.add(playsRank.get(genre).poll()[0]);
             }
         }
-        answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
+        int[] answer = new int[album.size()];
+        for (int i = 0; i < album.size(); i++)
+            answer[i] = album.get(i);
         return answer;
     }
 }
