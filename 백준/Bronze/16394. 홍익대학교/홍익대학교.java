fd0b7e359053895d7e7bb37d857799d5af68b230
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-27 23:54:38 +0900

    [Bronze V] Title: 홍익대학교, Time: 124 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(read() - 1946);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
