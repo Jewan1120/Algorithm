afa0173b18390a40a8ede46233140c70d8407248
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-28 00:23:40 +0900

    [level 2] Title: 테이블 해시 함수, Time: 10.26 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -5,9 +5,8 @@ class Solution {
         Arrays.sort(data, (o1, o2) -> o1[col - 1] != o2[col - 1] ? o1[col - 1] - o2[col - 1] : o2[0] - o1[0]);
         for (int i = row_begin; i <= row_end; i++) {
             int s = 0;
-            for (int j = 0; j < data[0].length; j++) {
+            for (int j = 0; j < data[0].length; j++)
                 s += data[i - 1][j] % i;
-            }
             answer ^= s;
         }
         return answer;
