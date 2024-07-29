b913b12a36816453dd1013da406d9fe1cc808d4d
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-29 17:04:56 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 89.93 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,33 +1,33 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
+        int answer = 0, n = queue1.length;
+        Deque<Integer> q1 = new ArrayDeque<>();
+        Deque<Integer> q2 = new ArrayDeque<>();
         long s1 = 0;
         long s2 = 0;
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        for(int i = 0; i < queue1.length; i ++){
-            q1.add(queue1[i]);
+        for (int i = 0; i < n; i++) {
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
             s1 += queue1[i];
-            q2.add(queue2[i]);
             s2 += queue2[i];
         }
-        while(s1 != s2){
-            if(s1 > s2){
-                s1 -= q1.peek();
-                s2 += q1.peek();
-                q2.add(q1.poll());
-            }else{
-                s2 -= q2.peek();
-                s1 += q2.peek();
-                q1.add(q2.poll());
+        while (s1 != s2) {
+            if (s1 > s2) {
+                int p = q1.pop();
+                q2.offer(p);
+                s1 -= p;
+                s2 += p;
+            } else if (s1 < s2) {
+                int p = q2.pop();
+                q1.offer(p);
+                s2 -= p;
+                s1 += p;
             }
             answer++;
-            if(answer == queue1.length * 3){
-                answer = -1;
-                break;
-            }
+            if (answer == n * 3)
+                return -1;
         }
         return answer;
     }
