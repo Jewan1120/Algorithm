d4c2585bbfdd208556d3d32380710395f71e992c
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-31 11:20:51 +0900

    [Silver II] Title: 최대 힙, Time: 212 ms, Memory: 18676 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,30 +1,25 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (; n > 0; n--) {
-            int p = Integer.parseInt(br.readLine());
-            if (p == 0) {
-                if (pq.isEmpty()){
-                    bw.write("0\n");
-                } else {
-                    bw.write(pq.poll() + "\n");
-                }
-            } else {
-                pq.add(p);
-            }
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            int p = read();
+            if (p == 0)
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            else
+                pq.offer(p);
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
