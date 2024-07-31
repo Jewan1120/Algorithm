827e96e645ec943b935d44908cb5cc6884985839
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-07-31 20:16:53 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 112 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println((int) Math.ceil(read() / 5f));
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
