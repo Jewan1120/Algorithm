f0d747bcb3f9aedddc9cf7e63619305b23ed609c
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-04 15:27:09 +0900

    [level 4] Title: 징검다리, Time: 46.85 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        Arrays.sort(rocks);
+        int l = 1, r = distance;
+        while (l <= r) {
+            int removed = 0;
+            int now = 0;
+            int m = (l + r) / 2;
+            for (int i = 0; i < rocks.length; i++) {
+                if (rocks[i] - now < m) {
+                    removed++;
+                } else {
+                    now = rocks[i];
+                }
+            }
+            if (distance - now < m)
+                removed++;
+            if (removed <= n) {
+                answer = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return answer;
+    }
+}
