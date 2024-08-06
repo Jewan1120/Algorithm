61c449dc853cb32b34b66a56f159719d398156e8
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-06 17:23:57 +0900

    [level 2] Title: N-Queen, Time: 302.47 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,39 +1,41 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
 class Solution {
+    
+    static boolean[][] board;
+    static int maxDepth = 0;
+    static int cnt = 0;
+    static int len = 0;
+    
     public int solution(int n) {
-        int answer = 0;
-        Queue<boolean[][]> boardQue = new ArrayDeque<boolean[][]>();
-        Queue<Integer> countQue = new ArrayDeque<Integer>();
-        boardQue.add(new boolean[n][n]);
-        countQue.add(0);
-        while (!boardQue.isEmpty()) {
-            boolean[][] board = boardQue.poll();
-            int count = countQue.poll();
-            for (int i = 0; i < n; i++) {
-                if (!board[count][i]) {
-                    boolean[][] tmpBoard = new boolean[n][n];
-                    for (int j = 0; j < board.length; j++) {
-                        tmpBoard[j] = board[j].clone();
-                    }
-                    if (count + 1 == n) {
-                        answer++;
-                        break;
-                    }
-                    for (int j = 1; j < n - count; j++) {
-                        tmpBoard[count + j][i] = true;
-                        if (0 <= i - j) {
-                            tmpBoard[count + j][i - j] = true;
-                        }
-                        if (i + j < n) {
-                            tmpBoard[count + j][i + j] = true;
-                        }
-                    }
-                    boardQue.add(tmpBoard.clone());
-                    countQue.add(count + 1);
-                }
+        board = new boolean[n][n];
+        maxDepth = n;
+        len = n;
+        recursive(0);
+        return cnt;
+    }
+    
+    private void recursive(int depth) {
+        if (depth == maxDepth) {
+            cnt++;
+            return;
+        }
+        for (int i = 0; i < len; i++) {
+            if (isPossible(depth, i)) {
+                board[depth][i] = true;
+                recursive(depth + 1);
+                board[depth][i] = false;
             }
         }
-        return answer;
+    }
+
+    private boolean isPossible(int depth, int x) {
+        for (int i = 1; i <= depth; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int ny = depth - i;
+                int nx = x + j * i;
+                if (0 <= ny && ny < len && 0 <= nx && nx < len && board[ny][nx])
+                    return false;
+            }
+        }
+        return true;
     }
 }
