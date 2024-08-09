48efbe5751bff92e7ee4d778ad5607fa4aabe213
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-09 23:03:33 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
