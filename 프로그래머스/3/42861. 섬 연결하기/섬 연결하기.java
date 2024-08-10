b03732116cbbefd1e75bc5558772817aeb94857a
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-10 17:37:43 +0900

    [level 3] Title: 섬 연결하기, Time: 1.08 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,57 +1,31 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-
 class Solution {
-    
-    class Edge implements Comparable<Edge> {
-        int to;
-        int weight;
-        
-        public Edge(int to, int weight){
-            this.to = to;
-            this.weight = weight;
-        }
-        
-        @Override
-        public int compareTo(Edge other){
-            return Integer.compare(this.weight, other.weight);
-        }
-    }
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        List<List<Edge>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++){
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-        for(int i = 0; i < costs.length; i++){
-            int x = costs[i][0];
-            int y = costs[i][1];
-            int weight = costs[i][2];
-            graph.get(x).add(new Edge(y, weight));
-            graph.get(y).add(new Edge(x, weight));
+        for (int[] cost : costs) {
+            int u = cost[0], v = cost[1], w = cost[2];
+            graph.get(u).add(new int[] { v, w });
+            graph.get(v).add(new int[] { u, w });
         }
-        boolean[] inMST = new boolean[n];
-        PriorityQueue<Edge> que = new PriorityQueue<>();
-        que.add(new Edge(0, 0));
-        while(!que.isEmpty()){
-            Edge cur = que.poll();
-            int u = cur.to;
-            if(inMST[u]) continue;
-            
-            inMST[u] = true;
-            answer += cur.weight;
-            
-            for(Edge neighbor : graph.get(u)){
-                int v = neighbor.to;
-                int weight = neighbor.weight;
-                if (!inMST[v]) {
-                    que.add(new Edge(v, weight));
-                }
+        boolean[] visited = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.offer(new int[] { 0, 0 });
+        while (!pq.isEmpty()) {
+            int[] u = pq.poll();
+            if (visited[u[0]])
+                continue;
+            visited[u[0]] = true;
+            answer += u[1];
+            for (int[] v : graph.get(u[0])) {
+                if (!visited[v[0]])
+                    pq.offer(new int[] { v[0], v[1] });
             }
         }
         return answer;
-    }   
+    }
 }
