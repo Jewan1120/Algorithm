53bbf62f64ee89b6e2829feaa6704abf93c55ade
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-14 18:47:25 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.25 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,51 +1,34 @@
 import java.util.Deque;
 import java.util.ArrayDeque;
-import java.util.HashMap;
 import java.util.ArrayList;
 class Solution {
-    static class Node {
-        int n;
-        int d;
-
-        public Node(int n, int d) {
-            this.n = n;
-            this.d = d;
-        }
-    }
-
     public int solution(int n, int[][] edge) {
-        boolean[] chk = new boolean[n + 1];
-        HashMap<Integer, ArrayList<Integer>> nodeMap = new HashMap<>();
-        for (int[] node : edge) {
-            if (!nodeMap.containsKey(node[0])) {
-                nodeMap.put(node[0], new ArrayList<>());
-            }
-            if (!nodeMap.containsKey(node[1])) {
-                nodeMap.put(node[1], new ArrayList<>());
-            }
-            nodeMap.get(node[0]).add(node[1]);
-            nodeMap.get(node[1]).add(node[0]);
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++)
+            graph.add(new ArrayList<>());
+        for (int i = 0; i < edge.length; i++) {
+            graph.get(edge[i][0]).add(edge[i][1]);
+            graph.get(edge[i][1]).add(edge[i][0]);
         }
-        Deque<Node> deq = new ArrayDeque<>();
-        chk[0] = chk[1] = true;
-        deq.push(new Node(1, 0));
-        int depth = 0;
-        ArrayList<Integer> nodeArr = new ArrayList<>();
-        while (!deq.isEmpty()) {
-            Node node = deq.pollFirst();
-            if (depth < node.d) {
-                depth = node.d;
-                nodeArr.clear();
+        Deque<int[]> dq = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
+        dq.offer(new int[] { 1, 1 });
+        int cnt = 0, maxDepth = 1;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            if (visited[cur[0]])
+                continue;
+            visited[cur[0]] = true;
+            if (maxDepth == cur[1])
+                cnt++;
+            else {
+                maxDepth = cur[1];
+                cnt = 1;
             }
-            nodeArr.add(node.n);
-            ArrayList<Integer> adjacent = nodeMap.get(node.n);
-            for (int i : adjacent) {
-                if (!chk[i]) {
-                    chk[i] = true;
-                    deq.offerLast(new Node(i, node.d + 1));
-                }
+            for (int v : graph.get(cur[0])) {
+                dq.offer(new int[] { v, cur[1] + 1 });
             }
         }
-        return nodeArr.size();
+        return cnt;
     }
 }
