79697589cc6070047aa3c21d780834a37e19c0de
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-15 14:53:47 +0900

    [level 3] Title: 순위, Time: 11.87 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,31 +2,28 @@ class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         boolean[][] graph = new boolean[n + 1][n + 1];
-        for (int[] result : results) {
-            int winner = result[0];
-            int loser = result[1];
-            graph[winner][loser] = true;
+        for (int i = 0; i < results.length; i++) {
+            int u = results[i][0], v = results[i][1];
+            graph[u][v] = true;
         }
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (graph[i][k] && graph[k][j]) {
+        for (int k = 1; k < n + 1; k++)
+            for (int i = 1; i < n + 1; i++)
+                for (int j = 1; j < n + 1; j++)
+                    if (graph[i][k] && graph[k][j])
                         graph[i][j] = true;
-                    }
-                }
-            }
-        }
-        next: for (int i = 1; i <= n; i++) {
-            boolean canDetermine = true;
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
+        for (int i = 1; i < n + 1; i++) {
+            boolean possible = true;
+            for (int j = 1; j < n + 1; j++) {
+                if (i == j)
+                    continue;
                 if (!graph[i][j] && !graph[j][i]) {
-                    continue next;
+                    possible = false;
+                    break;
                 }
             }
-            answer++;
+            if (possible)
+                answer++;
         }
-        
         return answer;
     }
 }
