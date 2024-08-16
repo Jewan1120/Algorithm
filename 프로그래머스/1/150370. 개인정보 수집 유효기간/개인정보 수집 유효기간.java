8e0230aa3e18f7f755e8d652f0babf69cdaf1ddb
백제완 <116615068+Jewan1120@users.noreply.github.com>
2024-08-16 17:16:48 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.66 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,50 +1,32 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        String[] dateTmp = today.split("\\.");
-        int[] dateChk = new int[3];
-        for(int i = 0; i < 3; i++){
-            dateChk[i] = Integer.parseInt(dateTmp[i]);
+        int intToday = strToInt(today);
+        HashMap<String, Integer> termsMap = new HashMap<>();
+        for (int i = 0; i < terms.length; i++) {
+            String[] StrArr = terms[i].split(" ");
+            termsMap.put(StrArr[0], Integer.parseInt(StrArr[1]));
         }
-        HashMap<String, Integer> termMap = new HashMap<>(terms.length);
-        for(String str : terms){
-            String[] part = str.split(" ");
-            termMap.put(part[0], Integer.parseInt(part[1]));
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            String[] StrArr = privacies[i].split(" ");
+            int privacyDate = strToInt(StrArr[0]) + termsMap.get(StrArr[1]) * 28 - 1;
+            if (intToday > privacyDate)
+                al.add(i + 1);
         }
-        for(int i = 0; i < privacies.length; i++){
-            String[] privacy = privacies[i].split(" ");
-            String[] part = privacy[0].split("\\.");
-            int[] date = new int[3];
-            for(int j = 0; j < 3; j++){
-                date[j] = Integer.parseInt(part[j]);
-            }
-            date[1] += termMap.get(privacy[1]);
-            while(date[1] > 12){
-                date[0]++;
-                date[1] -= 12;
-            }
-            date[2]--;
-            if(date[2] == 0 ){
-                date[1]--;
-                date[2] = 28;
-                if(date[1] == 0){
-                    date[0]--;
-                    date[1] = 12;
-                }
-            }
-            for(int j = 0; j < 3; j++){
-                if(date[j] < dateChk[j]){
-                    int[] tmp = new int[answer.length + 1];
-                    System.arraycopy(answer, 0, tmp, 0, answer.length);
-                    tmp[answer.length] = i + 1;
-                    answer = tmp;
-                    break;
-                }else if(date[j] > dateChk[j]){
-                    break;
-                }
-            }
-        }
+        int[] answer = new int[al.size()];
+        for (int i = 0; i < al.size(); i++)
+            answer[i] = al.get(i);
         return answer;
+    }
+
+    private int strToInt(String StrDate) {
+        int intDate = 0;
+        String[] strArr = StrDate.split("\\.");
+        intDate += (Integer.parseInt(strArr[0]) - 2000) * 12 * 28;
+        intDate += Integer.parseInt(strArr[1]) * 28;
+        intDate += Integer.parseInt(strArr[2]);
+        return intDate;
     }
 }
