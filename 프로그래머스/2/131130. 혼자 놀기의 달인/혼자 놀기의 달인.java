872a227233d21462c888dfe46fe6a843bb4c164f
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-08-28 13:03:27 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.06 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,30 +1,25 @@
-import java.util.ArrayList;
 class Solution {
     public int solution(int[] cards) {
-        int answer = 0;
-        boolean[] chk = new boolean[cards.length];
-        boolean flg = true;
-        ArrayList<ArrayList<Integer>> groups = new ArrayList<>();
-        while (flg) {
-            int idx = -1;
-            ArrayList<Integer> group = new ArrayList<>();
-            for (int i = 0; i < chk.length; i++) {
-                if (chk[i] == false) {
-                    idx = i;
+        boolean[] visited = new boolean[cards.length];
+        int c1 = 0, c2 = 0;
+        for (int i = 0; i < cards.length; i++) {
+            int cnt = 0;
+            if (!visited[i]) {
+                visited[i] = true;
+                int idx = cards[i] - 1;
+                cnt++;
+                while (!visited[idx]) {
+                    visited[idx] = true;
+                    idx = cards[idx] - 1;
+                    cnt++;
                 }
             }
-            if (idx == -1) {
-                break;
-            }
-            while (!chk[idx]) {
-                group.add(cards[idx]);
-                chk[idx] = true;
-                idx = cards[idx] - 1;
-            }
-            groups.add(group);
+            if (c1 < cnt) {
+                c2 = c1;
+                c1 = cnt;
+            } else
+                c2 = Math.max(c2, cnt);
         }
-        groups.sort(((o1, o2) -> o2.size() - o1.size()));
-        answer = groups.size() == 1 ? 0 : groups.get(0).size() * groups.get(1).size();
-        return answer;
+        return c1 * c2;
     }
 }
