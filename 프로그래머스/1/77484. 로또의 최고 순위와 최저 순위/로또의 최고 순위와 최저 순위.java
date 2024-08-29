c4294ff66b0925c0849a310b669ae7a23a3097ea
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-08-29 12:25:25 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,22 +1,22 @@
+import java.util.HashSet;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {7, 1};
-        next: for(int i : lottos){
-            if(i == 0){
-                answer[0]--;
-                answer[1]++;
-            }else{
-                for(int j : win_nums){
-                    if(i == j){
-                        answer[0]--;
-                        continue next;
-                    }
-                }
-                answer[1]++;
-            }
-        }
-        if(answer[0] == 7) answer = new int[] {6, 6};
-        if(answer[1] == 7) answer = new int[] {1, 6};
-        return answer;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < 6; i++)
+            hs.add(win_nums[i]);
+        int cnt = 0, zero = 0;
+        for (int i = 0; i < 6; i++)
+            if (hs.contains(lottos[i]))
+                cnt++;
+            else if (lottos[i] == 0)
+                zero++;
+        if (cnt == 6)
+            return new int[] { 1, 1 };
+        else if (zero == 6)
+            return new int[] { 1, 6 };
+        else if(cnt == 0 && zero == 0)
+            return new int[] { 6, 6 };
+        else
+            return new int[] { 7 - (cnt + zero), 7 - cnt };
     }
 }
