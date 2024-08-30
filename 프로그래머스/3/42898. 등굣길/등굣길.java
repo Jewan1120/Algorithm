ed379de3a0303b6a9af8515b44acffd63dcd2465
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-08-30 14:13:55 +0900

    [level 3] Title: 등굣길, Time: 1.16 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,18 +1,17 @@
 class Solution {
+    int MOD = 1_000_000_007;
     public int solution(int m, int n, int[][] puddles) {
         int[][] board = new int[n + 1][m + 1];
+        for (int i = 0; i < puddles.length; i++)
+            board[puddles[i][1]][puddles[i][0]] = -1;
         board[0][1] = 1;
-        for(int[] puddle : puddles){
-            board[puddle[1]][puddle[0]] = -1;
-        }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(board[i][j] == -1) continue;
-                int y = board[i - 1][j] == -1 ? 0 : board[i - 1][j];
-                int x = board[i][j - 1] == -1 ? 0 : board[i][j - 1];
-                board[i][j] = (x + y) % 1_000_000_007;
-            }
-        }
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                if (board[i][j] != -1) {
+                    int u = Math.max(0, board[i - 1][j]);
+                    int l = Math.max(0, board[i][j - 1]);
+                    board[i][j] = (u + l) % MOD;
+                }
         return board[n][m];
     }
 }
