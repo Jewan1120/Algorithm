f02702177687ac610f565a082a71516236237f1d
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-07 20:21:21 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 364 ms, Memory: 22636 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -24,7 +24,7 @@ public class Main {
         int c, n = System.in.read() & 15;
         boolean m = n == 13;
         if (m)
-            n = System.in.read();
+            n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
         return m ? ~n + 1 : n;
