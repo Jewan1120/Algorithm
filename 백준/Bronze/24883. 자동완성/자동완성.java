07987662c47d0e9cdb164550ea59da1aed0479d4
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-11 21:25:57 +0900

    [Bronze V] Title: 자동완성, Time: 100 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int c = System.in.read();
+        if(c == 'N' || c == 'n')
+            System.out.println("Naver D2");
+        else
+            System.out.println("Naver Whale");
+    }
+}
