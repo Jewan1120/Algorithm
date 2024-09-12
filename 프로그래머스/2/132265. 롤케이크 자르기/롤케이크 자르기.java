c322828b085ed9f91ef26b47ff93f77bae26c9ea
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-12 09:15:08 +0900

    [level 2] Title: 롤케이크 자르기, Time: 121.11 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,19 +2,20 @@ import java.util.HashSet;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        HashSet<Integer> typeSetF = new HashSet<>();
-        HashSet<Integer> typeSetB = new HashSet<>();
-        int[] typeArrF = new int[topping.length];
-        int[] typeArrB = new int[topping.length];
-        for (int i = 0; i < topping.length; i++) {
-            typeSetF.add(topping[i]);
-            typeArrF[i] = typeSetF.size();
-            typeSetB.add(topping[topping.length - 1 - i]);
-            typeArrB[topping.length - 1 - i] = typeSetB.size();
+        int len = topping.length;
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+        int[] typeA = new int[len];
+        int[] typeB = new int[len];
+        for (int i = 0; i < len; i++) {
+            setA.add(topping[i]);
+            setB.add(topping[len - 1 - i]);
+            typeA[i] = setA.size();
+            typeB[len - 1 - i] = setB.size();
         }
-        for (int i = 0; i < typeArrF.length - 1; i++) {
-            if (typeArrF[i] == typeArrB[i + 1]) answer++;
-        }
+        for (int i = 0; i < len - 1; i++)
+            if (typeA[i] == typeB[i + 1])
+                answer++;
         return answer;
     }
 }
