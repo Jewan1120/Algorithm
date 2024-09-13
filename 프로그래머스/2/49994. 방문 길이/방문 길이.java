4f7fe76b9f4cdb9b318a5a4327cb7c5579773759
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-13 09:52:31 +0900

    [level 2] Title: 방문 길이, Time: 0.32 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,41 +1,41 @@
-import java.util.HashSet;
 class Solution {
+    boolean[][][] board = new boolean[11][11][4];
+    int[] dy = { 1, -1, 0, 0 };
+    int[] dx = { 0, 0, 1, -1 };
+    char[] dirsArr;
+    int area;
+    
     public int solution(String dirs) {
-        int answer = 0;
-        int[] p = new int[2];
-        HashSet<String> hs = new HashSet<>();
-        for(String dir : dirs.split("")){
-            StringBuilder s = new StringBuilder(dir).append(p[0]).append(p[1]);
-            StringBuilder e = new StringBuilder();
-            switch(dir){
-                case "U" :
-                    if(p[0] >= 5) continue;
-                    p[0]++;
-                    e.append("D");
-                    break;
-                case "D" :
-                    if(p[0] <= -5) continue;
-                    p[0]--;
-                    e.append("U");
-                    break;
-                case "R" :
-                    if(p[1] >= 5) continue;
-                    p[1]++;
-                    e.append("L");
-                    break;
-                case "L" :
-                    if(p[1] <= -5) continue;
-                    p[1]--;
-                    e.append("R");
-                    break;
-            }
-            e.append(p[0]).append(p[1]);
-            if(!hs.contains(s.toString())){
-                answer++;
-                hs.add(s.toString());
-                hs.add(e.toString());
-            }
+        dirsArr = dirs.toCharArray();
+        recursion(5, 5, 0);
+        return area;
+    }
+    private void recursion(int y, int x, int depth) {
+        if (depth == dirsArr.length)
+            return;
+        int to = 0, from = 1;
+        if (dirsArr[depth] == 'D') {
+            to = 1;
+            from = 0;
+        } else if (dirsArr[depth] == 'R') {
+            to = 2;
+            from = 3;
+        } else if (dirsArr[depth] == 'L') {
+            to = 3;
+            from = 2;
         }
-        return answer;
+        int ny = y + dy[to], nx = x + dx[to];
+        board[y][x][to] = true;
+        if (isValid(ny, nx)) {
+            if (!board[ny][nx][from])
+                area++;
+            board[ny][nx][from] = true;
+            recursion(ny, nx, depth + 1);
+        } else
+            recursion(y, x, depth + 1);
+    }
+
+    private boolean isValid(int y, int x) {
+        return 0 <= y && y < 11 && 0 <= x && x < 11;
     }
 }
