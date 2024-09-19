aef8897d75f06050665c1398b9d8f6ac1c404ed8
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-19 10:58:30 +0900

    [level 2] Title: 주식가격, Time: 33.04 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,18 @@
 import java.util.Stack;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Stack<int []> stock = new Stack<>();
-        for(int i = 0; i < prices.length; i++){
-            int[] stockArr = {prices[i], prices.length - 1 - i};
-            answer[i] = stockArr[1];
-            if(stock.isEmpty() || stock.peek()[0] <= stockArr[0]){
-                stock.push(stockArr);
-            }else{
-                while(!stock.isEmpty() && stock.peek()[0] > stockArr[0]){
-                    int[] tmp = stock.pop();
-                    answer[prices.length - 1 - tmp[1]] = tmp[1] - stockArr[1];
-                }
-                stock.push(stockArr);
-            }
+        int n = prices.length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = n - 1 - i;
+        }
+        Stack<Integer> stk = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stk.isEmpty() && prices[stk.peek()] >= prices[i])
+                stk.pop();
+            if (!stk.isEmpty())
+                answer[i] -= n - 1 - stk.peek();
+            stk.push(i);
         }
         return answer;
     }
