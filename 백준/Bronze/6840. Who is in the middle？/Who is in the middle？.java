8cd2b7856711269882024c0d2bfb3963024fce0a
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-09-22 23:37:36 +0900

    [Bronze V] Title: Who is in the middle?, Time: 92 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = { read(), read(), read() };
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
