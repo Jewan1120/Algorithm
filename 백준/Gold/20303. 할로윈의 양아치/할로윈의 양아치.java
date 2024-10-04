2607b69445daad3f4c1b5130f5e638807ee8134b
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-04 18:12:54 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 668 ms, Memory: 560160 KB -BaekjoonHub

---
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -0,0 +1,73 @@
+public class Main {
+
+    static int[] parent;
+    static int[] rank;
+
+    public static void main(String[] args) throws Exception {
+        int n = read(), m = read(), k = read();
+        int[] candy = new int[n + 1];
+        int[] cnt = new int[n + 1];
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            candy[i] = read();
+            parent[i] = i;
+            cnt[i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            int x = read(), y = read();
+            union(x, y);
+        }
+        int len = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (parent[i] != i) {
+                int root = find(i);
+                candy[root] += candy[i];
+                cnt[root]++;
+            } else
+                len++;
+        }
+        int[][] group = new int[len + 1][2];
+        int idx = 0;
+        for (int i = 1; i < n + 1; i++)
+            if (parent[i] == i)
+                group[++idx] = new int[] { cnt[i], candy[i] };
+        int[][] dp = new int[len + 1][k];
+        for (int i = 1; i <= len; i++)
+            for (int j = 0; j < k; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - group[i][0] >= 0)
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - group[i][0]] + group[i][1]);
+            }
+        System.out.println(dp[len][k - 1]);
+    }
+
+    private static int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY)
+            if (rank[rootX] < rank[rootY])
+                parent[rootX] = rootY;
+            else if (rank[rootX] > rank[rootY])
+                parent[rootY] = rootX;
+            else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
