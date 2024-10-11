ac957c51b233d1fea16e6ac059f3b9cafc82f998
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-11 09:58:21 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 132 ms, Memory: 15456 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -7,48 +6,55 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
-        next: for (; t > 0; t--) {
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            int[][] p = new int[n + 2][2];
+            StringTokenizer st;
+            int[][] dist = new int[n + 2][2];
             for (int i = 0; i < n + 2; i++) {
                 st = new StringTokenizer(br.readLine());
-                p[i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+                dist[i][0] = Integer.parseInt(st.nextToken());
+                dist[i][1] = Integer.parseInt(st.nextToken());
             }
-            boolean[][] dist = new boolean[n + 2][n + 2];
-            for (int i = 0; i < n + 2; i++) {
+            boolean[][] possible = new boolean[n + 2][n + 2];
+            for (int i = 0; i < n + 2; i++)
                 for (int j = 0; j < n + 2; j++) {
-                    if (i != j) {
-                        int d = Math.abs(p[i][0] - p[j][0]) + Math.abs(p[i][1] - p[j][1]);
-                        if (d <= 1000) {
-                            dist[i][j] = true;
-                        }
-                    }
+                    if (canGo(dist[i], dist[j]))
+                        possible[i][j] = true;
                 }
+            if (isReachable(possible)) {
+                sb.append("happy");
+            } else {
+                sb.append("sad");
             }
-            boolean[] visited = new boolean[n + 2];
-            Deque<Integer> dq = new ArrayDeque<>();
-            dq.offer(0);
-            visited[0] = true;
-            while (!dq.isEmpty()) {
-                int cur = dq.poll();
-                if (cur == n + 1) {
-                    sb.append("happy").append("\n");
-                    continue next;
-                }
-                for (int i = 0; i < n + 2; i++) {
-                    if (dist[cur][i] && !visited[i]) {
-                        dq.offer(i);
-                        visited[i] = true;
-                    }
-                }
-            }
-            sb.append("sad").append("\n");
+            sb.append("\n");
         }
         System.out.println(sb);
+    }
+
+    private static boolean canGo(int[] A, int[] B) {
+        return Math.abs(A[0] - B[0]) + Math.abs(A[1] - B[1]) <= 1000;
+    }
+
+    private static boolean isReachable(boolean[][] possible) {
+        int len = possible.length;
+        Deque<Integer> dq = new ArrayDeque<>();
+        boolean[] visited = new boolean[len];
+        dq.offer(0);
+        visited[0] = true;
+        while (!dq.isEmpty()) {
+            int cur = dq.poll();
+            if (cur == len - 1) 
+                return true;
+            for (int i = 0; i < len; i++) 
+                if (possible[cur][i] && !visited[i]) {
+                    dq.offer(i);
+                    visited[i] = true;
+                }
+        }
+        return false;
     }
 }
