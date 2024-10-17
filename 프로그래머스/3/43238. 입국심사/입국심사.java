e48fe951dc8926e612a5819d227c353e11750096
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-17 09:12:09 +0900

    [level 3] Title: 입국심사, Time: 114.77 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,25 @@
 import java.util.Arrays;
+
 class Solution {
-    public static long solution(int n, int[] times) {
-        long answer = 0;
+    public long solution(int n, int[] times) {
         Arrays.sort(times);
-        long left = 1;
-        long right = (long) n * times[times.length - 1];
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long count = 0;
-            for (int time : times) {
-                count += mid / time;
-            }
-            if (count >= n) {
-                answer = mid;
-                right = mid - 1;
+        long l = 1, r = (long) times[times.length - 1] * n;
+        while (l <= r) {
+            long m = (l + r) / 2;
+            if (isPossible(times, n, m)) {
+                r = m - 1;
             } else {
-                left = mid + 1;
+                l = m + 1;
             }
         }
-        return answer;
+        return l;
+    }
+
+    private boolean isPossible(int[] times, int n, long target) {
+        long cnt = 0;
+        for (int time : times) {
+            cnt += target / (long) time;
+        }
+        return cnt >= n;
     }
 }
