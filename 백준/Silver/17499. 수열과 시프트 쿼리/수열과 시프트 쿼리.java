10c1a901df0a1057775dc40b80654225d42c43d7
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-17 10:36:19 +0900

    [Silver III] Title: 수열과 시프트 쿼리, Time: 300 ms, Memory: 22748 KB -BaekjoonHub

--- 백준/Silver/17499. 수열과 시프트 쿼리/수열과 시프트 쿼리.java
+++ 백준/Silver/17499. 수열과 시프트 쿼리/수열과 시프트 쿼리.java
@@ -1,27 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
+        int n = read(), m = read();
         int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < n; i++)
+            arr[i] = read();
         int idx = 0;
-        while (q-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int oper = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
+        while (m-- > 0) {
+            int oper = read();
+            int s = read();
             if (oper == 1) {
-                int p = Integer.parseInt(st.nextToken());
-                arr[(idx + s - 1 + n) % n] += p;
+                int p = read();
+                arr[(idx + s - 1) % n] += p;
             } else if (oper == 2) {
                 idx = (idx - s + n) % n;
             } else {
@@ -33,5 +23,17 @@ public class Main {
             sb.append(arr[(idx + i) % n]).append(" ");
         }
         System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)
+            n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return m ? ~n + 1 : n;
     }
 }
