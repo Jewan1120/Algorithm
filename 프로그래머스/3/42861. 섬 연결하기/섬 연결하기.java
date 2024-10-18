e1b8bc6620be1a6fc7cb622c9c7df898fd097dbb
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-18 09:11:31 +0900

    [level 3] Title: 섬 연결하기, Time: 1.45 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,29 +1,39 @@
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 class Solution {
+    
+    class Edge {
+        int to, weight;
+
+        Edge(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        ArrayList<ArrayList<Edge>> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            edges.add(new ArrayList<>());
         for (int[] cost : costs) {
-            int u = cost[0], v = cost[1], w = cost[2];
-            graph.get(u).add(new int[] { v, w });
-            graph.get(v).add(new int[] { u, w });
+            edges.get(cost[0]).add(new Edge(cost[1], cost[2]));
+            edges.get(cost[1]).add(new Edge(cost[0], cost[2]));
         }
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
         boolean[] visited = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.offer(new int[] { 0, 0 });
-        while (!pq.isEmpty()) {
-            int[] u = pq.poll();
-            if (visited[u[0]])
+        pq.offer(new Edge(0, 0));
+        int cnt = 0;
+        while (!pq.isEmpty() && cnt < n) {
+            Edge cur = pq.poll();
+            if (visited[cur.to])
                 continue;
-            visited[u[0]] = true;
-            answer += u[1];
-            for (int[] v : graph.get(u[0])) {
-                if (!visited[v[0]])
-                    pq.offer(new int[] { v[0], v[1] });
+            visited[cur.to] = true;
+            cnt++;
+            answer += cur.weight;
+            for (Edge edge : edges.get(cur.to)) {
+                if (!visited[edge.to])
+                    pq.offer(edge);
             }
         }
         return answer;
