8a11a56c3c61a717f74d28b39dd80c6c5f6f075a
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-24 09:34:21 +0900

    [level 3] Title: 최고의 집합, Time: 0.55 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] solution(int n, int s) {
         int[] answer = new int[n];
-        if (s / n < 1) {
-            return new int[] {-1};
-        }
-        int mod = s % n;
+        if (n > s)
+            return new int[] { -1 };
         for (int i = n - 1; i >= 0; i--) {
             answer[i] = s / n;
-            if (mod > 0) {
+            if (s % n != 0)
                 answer[i]++;
-                mod--;
-            }
+            n--;
+            s -= answer[i];
         }
         return answer;
     }
