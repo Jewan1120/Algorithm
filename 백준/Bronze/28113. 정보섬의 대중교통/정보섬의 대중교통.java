8c9753125a87793f85df08cec90071857cd0a3db
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-24 23:17:47 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read(), a = read(), b = read();
+        if (a == b)
+            System.out.print("Anything");
+        else if (a < b)
+            System.out.print("Bus");
+        else
+            System.out.print("Subway");
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
