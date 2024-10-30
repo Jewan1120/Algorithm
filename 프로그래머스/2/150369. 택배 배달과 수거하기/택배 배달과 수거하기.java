08043ddcdf3728a7bfb8cf9ddb8a8930a975ea64
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-10-31 01:23:45 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 23.24 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,24 +1,16 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int deliveryOverLoad = 0;
-        int pickUpOverLoad = 0;
+        int delivery = 0, pickup = 0;
         for (int i = n - 1; i >= 0; i--) {
             int cnt = 0;
-            if (deliveries[i] > 0 || pickups[i] > 0) {
-                deliveries[i] -= deliveryOverLoad;
-                pickups[i] -= pickUpOverLoad;
-                while (deliveries[i] > 0 || pickups[i] > 0) {
-                    cnt++;
-                    deliveries[i] -= cap;
-                    pickups[i] -= cap;
-                }
-                deliveryOverLoad = -(deliveries[i]);
-                pickUpOverLoad = -(pickups[i]);
-                deliveries[i] = 0;
-                pickups[i] = 0;
-
+            while (deliveries[i] > delivery || pickups[i] > pickup) {
+                cnt++;
+                delivery += cap;
+                pickup += cap;
             }
+            delivery -= deliveries[i];
+            pickup -= pickups[i];
             answer += (i + 1) * 2 * cnt;
         }
         return answer;
