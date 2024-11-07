99281ac0753e73f760b44c61d5018e6ba262484e
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-07 12:37:28 +0900

    [level 3] Title: 징검다리 건너기, Time: 52.26 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,27 +1,18 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = Integer.MAX_VALUE;
-        Deque<Integer> deq = new LinkedList<>();
-        int i;
-        for(i = 0; i < k; i++){
-            while(!deq.isEmpty() && stones[i] >= stones[deq.peekLast()]){
-                deq.removeLast();
-            }
-            deq.addLast(i);
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < stones.length; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() == i - k)
+                dq.pollFirst();
+            while (!dq.isEmpty() && stones[dq.peekLast()] <= stones[i])
+                dq.pollLast();
+            dq.offerLast(i);
+            if (i >= k - 1)
+                answer = Math.min(answer, stones[dq.peekFirst()]);
         }
-        for(; i < stones.length; i++){
-            if(answer > stones[deq.peek()]) answer = stones[deq.peek()];
-            while(!deq.isEmpty() && deq.peek() <= i - k){
-                deq.removeFirst();
-            }
-            while(!deq.isEmpty() && stones[i] >= stones[deq.peekLast()]){
-                deq.removeLast();
-            }
-            deq.addLast(i);
-        }
-        if(answer > stones[deq.peek()]) answer = stones[deq.peek()];
         return answer;
     }
 }
