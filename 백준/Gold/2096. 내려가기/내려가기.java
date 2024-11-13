0b06b1b33ab93b855c2f62e1859b6ef0c82fe018
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-13 09:41:48 +0900

    [Gold V] Title: 내려가기, Time: 152 ms, Memory: 23548 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,39 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] maxDp = new int[n][3];
-        int[][] minDp = new int[n][3];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int[] maxDp = new int[3];
+        int[] minDp = new int[3];
         for (int i = 0; i < 3; i++) {
-            int p = Integer.parseInt(st.nextToken());
-            maxDp[0][i] = p;
-            minDp[0][i] = p;
+            int p = read();
+            maxDp[i] = p;
+            minDp[i] = p;
         }
-        int[] d = {-1, 0, 1};
         for (int i = 1; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            Arrays.fill(minDp[i], Integer.MAX_VALUE);
-            for (int j = 0; j < 3; j++) {
-                int p = Integer.parseInt(st.nextToken());
-                for (int k = 0; k < 3; k++) {
-                    if (0 <= j + d[k] && j + d[k] < 3) {
-                        maxDp[i][j] = Math.max(maxDp[i][j], p + maxDp[i - 1][j + d[k]]);
-                        minDp[i][j] = Math.min(minDp[i][j], p + minDp[i - 1][j + d[k]]);
-                    }
-                }
-            }
+            int[] next = { read(), read(), read() };
+            int[] nextMaxDp = new int[3];
+            int[] nextMinDp = new int[3];
+
+            nextMaxDp[0] = next[0] + Math.max(maxDp[0], maxDp[1]);
+            nextMaxDp[1] = next[1] + Math.max(Math.max(maxDp[0], maxDp[1]), maxDp[2]);
+            nextMaxDp[2] = next[2] + Math.max(maxDp[1], maxDp[2]);
+
+            nextMinDp[0] = next[0] + Math.min(minDp[0], minDp[1]);
+            nextMinDp[1] = next[1] + Math.min(Math.min(minDp[0], minDp[1]), minDp[2]);
+            nextMinDp[2] = next[2] + Math.min(minDp[1], minDp[2]);
+
+            maxDp = nextMaxDp;
+            minDp = nextMinDp;
         }
-        int max = Math.max(maxDp[n - 1][0], Math.max(maxDp[n - 1][1], maxDp[n - 1][2]));
-        int min = Math.min(minDp[n - 1][0], Math.min(minDp[n - 1][1], minDp[n - 1][2]));
-        System.out.printf("%d %d", max, min);
-        br.close();
+        StringBuilder sb = new StringBuilder();
+        sb.append(Math.max(maxDp[0], Math.max(maxDp[1], maxDp[2])));
+        sb.append(" ").append(Math.min(minDp[0], Math.min(minDp[1], minDp[2])));
+        System.out.println(sb);
+    }
+    
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
     }
 }
