b4c51361af455e782076ec60e47ce8f55bc05f53
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-15 19:47:18 +0900

    [level 2] Title: 요격 시스템, Time: 368.36 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.ArrayList;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
@@ -7,8 +6,8 @@ class Solution {
         int missile = 0;
         for (int i = 0; i < targets.length; i++) {
             if (missile <= targets[i][0]) {
-                missile = targets[i][1];
                 answer++;
+                missile = targets[i][1];
             }
         }
         return answer;
