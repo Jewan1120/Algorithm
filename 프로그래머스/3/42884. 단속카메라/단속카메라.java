db3aa5e0b058794d85bbf08144fca832f7c70fd0
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-15 20:07:00 +0900

    [level 3] Title: 단속카메라, Time: 8.85 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        int camera = Integer.MIN_VALUE;
         Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
-        for(int i = 0; i < routes.length; i++){
-            if(camera < routes[i][0]){
+        int camera = Integer.MIN_VALUE;
+        for (int i = 0; i < routes.length; i++) {
+            if (camera < routes[i][0]) {
                 answer++;
                 camera = routes[i][1];
             }
