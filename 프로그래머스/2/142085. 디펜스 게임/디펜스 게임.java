dcac874468b178393ecef11451ef7995ccf1b389
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-21 09:27:18 +0900

    [level 2] Title: 디펜스 게임, Time: 98.13 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,20 +1,19 @@
 import java.util.PriorityQueue;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        int answer = 0;
-        PriorityQueue<Integer> que = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int i = 0; i < enemy.length; i++) {
-            if (n < enemy[i] && k == 0) {
-                break;
-            }
-            que.offer(enemy[i]);
-            if (n < enemy[i] && 0 < k) {
-                n += que.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        int round = 0;
+        int len = enemy.length;
+        for (; round < len; round++) {
+            pq.offer(enemy[round]);
+            if (n < enemy[round]) {
+                if (0 == k)
+                    break;
+                n += pq.poll();
                 k--;
-            } 
-            n -= enemy[i];
-            answer++;
+            }
+            n -= enemy[round];
         }
-        return answer;
+        return round;
     }
 }
