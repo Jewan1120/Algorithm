5831bfb91a8aae085830ac773090ce445bdd470c
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-21 10:11:06 +0900

    [Gold III] Title: 텀 프로젝트, Time: 504 ms, Memory: 44912 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,50 +1,52 @@
 public class Main {
 
+    static int n;
     static int cnt;
     static int[] arr;
+
     static boolean[] visited;
-    static boolean[] cycle;
+    static boolean[] isCycle;
 
     public static void main(String[] args) throws Exception {
         int t = read();
         StringBuilder sb = new StringBuilder();
         while (t-- > 0) {
-            int n = read();
+            n = read();
             cnt = 0;
             arr = new int[n + 1];
             visited = new boolean[n + 1];
-            cycle = new boolean[n + 1];
-            for (int i = 1; i < n + 1; i++)
+            isCycle = new boolean[n + 1];
+            for (int i = 1; i <= n; i++)
                 arr[i] = read();
-            for (int i = 1; i < n + 1; i++) {
-                if (!visited[i]) {
-                    dfs(i);
-                }
-            }
+            for (int i = 1; i <= n; i++)
+                if (!visited[i])
+                    recurse(i);
             sb.append(n - cnt).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static void dfs(int node) {
-        visited[node] = true;
-        int next = arr[node];
+    private static void recurse(int cur) {
+        visited[cur] = true;
+        int next = arr[cur];
         if (!visited[next]) {
-            dfs(next);
-        } else if (!cycle[next]) {
+            recurse(next);
+        } else if (!isCycle[next]) {
             cnt++;
-            while (next != node) {
+            while (next != cur) {
                 cnt++;
                 next = arr[next];
             }
         }
-        cycle[node] = true;
+        isCycle[cur] = true;
     }
 
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
