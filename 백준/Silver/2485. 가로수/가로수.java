4cd96905913aaa2fd8797a575a1e488662f4f6d2
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-26 18:57:18 +0900

    [Silver IV] Title: 가로수, Time: 128 ms, Memory: 14732 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,23 +1,29 @@
 public class Main {
+
     public static void main(String[] args) throws Exception {
         int n = read();
         int[] arr = new int[n];
-        arr[0] = read();
-        int min = 0;
-        for (int i = 1; i < n; i++) {
+        int gcd = 0;
+        for (int i = 0; i < n; i++)
             arr[i] = read();
-            min = gcd(min, arr[i] - arr[i - 1]);
+        for (int i = 1; i < n; i++) {
+            gcd = GCD(gcd, arr[i] - arr[i - 1]);
         }
-        System.out.println((arr[n - 1] - arr[0]) / min - (n - 1));
+        System.out.println((arr[n - 1] - arr[0]) / gcd - (n - 1));
     }
-    private static int gcd(int a, int b) {
-        if (a % b == 0) return b;
-        return gcd(b, a % b);
+
+    private static int GCD(int a, int b) {
+        if (b == 0)
+            return a;
+        return GCD(b, a % b);
     }
+
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
