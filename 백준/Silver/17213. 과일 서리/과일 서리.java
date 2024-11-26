b49f931fbe611b848df170769f5036b4cdd66d84
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-26 18:23:11 +0900

    [Silver II] Title: 과일 서리, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read(), m = read();
+        long result = 1;
+        for (int i = 0; i < n - 1; i++) {
+            result *= (long) m - 1 - i;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            result /= (long) n - 1 - i;
+        }
+        System.out.println(result);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
