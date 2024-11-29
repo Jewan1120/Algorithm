da1f76f9b717af0d65cbee67f798616cebde9545
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-30 04:45:21 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 16.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,24 +1,27 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 class Solution {
+    
     public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
-        ArrayList<String[]> al = new ArrayList<>(files.length);
-        for(int i = 0; i < files.length; i++){
-            String[] tmp = new String[3];
-            StringBuilder head = new StringBuilder();
-            StringBuilder number = new StringBuilder();
-            for(char c : files[i].toCharArray()){
-                if(number.length() != 0 && !Character.isDigit(c)) break;
-                if(!Character.isDigit(c)) head.append(c);
-                else number.append(c);           
+        String regex = "^([^0-9]+)([0-9]{1,5}).*$";
+        Pattern pattern = Pattern.compile(regex);
+        String[][] strArr = new String[files.length][3];
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i];
+            Matcher matcher = pattern.matcher(fileName);
+            if (matcher.find()) {
+                String header = matcher.group(1).toLowerCase();
+                String number = matcher.group(2);
+                strArr[i] = new String[] { fileName, header, number };
             }
-            tmp[0] = String.valueOf(i);
-            tmp[1] = head.toString().toLowerCase();
-            tmp[2] = number.toString();
-            al.add(tmp);
         }
-        al.sort((s1, s2) -> s1[1].equals(s2[1]) ? Integer.parseInt(s1[2]) - Integer.parseInt(s2[2]) : s1[1].compareTo(s2[1]));
-        for(int i = 0; i < answer.length; i++) answer[i] = files[Integer.parseInt(al.get(i)[0])];
+        Arrays.sort(strArr, (s1, s2) -> !s1[1].equals(s2[1]) ? s1[1].compareTo(s2[1])
+                : Integer.parseInt(s1[2]) - Integer.parseInt(s2[2]));
+        String[] answer = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            answer[i] = strArr[i][0];
+        }
         return answer;
     }
 }
