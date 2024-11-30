9853204a6f49537d84e44cfa00f30c6e5f1483c3
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-30 14:01:18 +0900

    [Silver IV] Title: ATM, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,25 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
         int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (; n > 0; n--) {
-            arr[n - 1] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < n; i++)
+            arr[i] = read();
         Arrays.sort(arr);
-        int result = 0;
-        int time = 0;
-        for (int i : arr) {
-            time += i;
-            result += time;
+        int total = 0, time = 0;
+        for (int i = 0; i < n; i++) {
+            time += arr[i];
+            total += time;
         }
-        System.out.println(result);
+        System.out.println(total);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
     }
 }
