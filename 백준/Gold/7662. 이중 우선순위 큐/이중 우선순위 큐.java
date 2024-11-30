c7b3d72c44ee833b0566c8c5efe46b883c8a8410
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-11-30 16:05:49 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2508 ms, Memory: 359720 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,51 +1,40 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
-        for (; t > 0; t--) {
-            TreeMap<Integer, Integer> ts = new TreeMap<>();
-            StringTokenizer st;
-            int k = Integer.parseInt(br.readLine());
-            for (; k > 0; k--) {
-                st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            int q = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> tm = new TreeMap<>();
+            while (q-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 String oper = st.nextToken();
-                int n = Integer.parseInt(st.nextToken());
-                if (oper.equals("I")) {
-                    ts.putIfAbsent(n, 0);
-                    ts.replace(n, ts.get(n) + 1);
-                } else if (!ts.isEmpty()) {
-                    int key;
-                    if (n == 1) {
-                        key = ts.lastKey();
-                        ts.replace(key, ts.get(key) - 1);
-                    } else {
-                        key = ts.firstKey();
-                        ts.replace(key, ts.get(key) - 1);
-                    }
-                    if (ts.get(key) == 0) {
-                        ts.remove(key);
-                    }
+                int number = Integer.parseInt(st.nextToken());
+                if (oper.equals("I"))
+                    tm.put(number, tm.getOrDefault(number, 0) + 1);
+                else if (!tm.isEmpty()) {
+                    int key = 0;
+                    if (number > 0)
+                        key = tm.lastKey();
+                    else
+                        key = tm.firstKey();
+                    tm.put(key, tm.get(key) - 1);
+                    if (tm.get(key) == 0)
+                        tm.remove(key);
                 }
             }
-            if (ts.isEmpty()) {
-                bw.write("EMPTY");
-            } else {
-                bw.write(ts.lastKey() + " " + ts.firstKey());
-            }
-            bw.newLine();
+            if (tm.isEmpty())
+                sb.append("EMPTY");
+            else
+                sb.append(tm.lastKey()).append(" ").append(tm.firstKey());
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
