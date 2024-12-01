0a308285c11674e5afe4bb6d7f093fc54601dcb5
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-01 16:44:52 +0900

    [level 2] Title: 가장 큰 수, Time: 202.82 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,14 +2,12 @@ import java.util.Arrays;
 class Solution {
     public String solution(int[] numbers) {
         String[] strArr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++)
             strArr[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(strArr, (o1, o2) -> Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2));
+        Arrays.sort(strArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         StringBuilder sb = new StringBuilder();
-        for (String str : strArr) {
+        for (String str : strArr)
             sb.append(str);
-        }
         return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
