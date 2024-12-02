6f30329072dc8438768b2ac672cba4dc331ddafb
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-02 17:06:36 +0900

    [Silver II] Title: 아이들과 선물 상자, Time: 216 ms, Memory: 20560 KB -BaekjoonHub

--- 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.java
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.java
@@ -1,33 +1,29 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.PriorityQueue;
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
         int n = read(), m = read();
-        PriorityQueue<Integer> boxQue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
         for (int i = 0; i < n; i++)
-            boxQue.offer(read());
-        int[] wishList = new int[m];
-        for (int i = 0; i < m; i++)
-            wishList[i] = read();
-        boolean flg = false;
+            pq.offer(read());
         for (int i = 0; i < m; i++) {
-            int req = wishList[i];
-            if (boxQue.isEmpty() || boxQue.peek() < req) {
-                flg = true;
-                break;
+            int w = read();
+            if (pq.peek() < w) {
+                System.out.println(0);
+                return;
             }
-            int gift = boxQue.poll();
-            gift -= req;
-            if (gift > 0)
-                boxQue.offer(gift);
+            pq.offer(pq.poll() - w);
         }
-        System.out.println(flg ? 0 : 1);
+        System.out.println(1);
     }
+
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
