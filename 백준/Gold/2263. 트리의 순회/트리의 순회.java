c8801926f6aedea4c0a1744747f90289e7fd8224
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-02 18:50:34 +0900

    [Gold I] Title: 트리의 순회, Time: 1336 ms, Memory: 24112 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] inOrder, postOrder;
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        inOrder = new int[n];
+        for (int i = 0; i < n; i++)
+            inOrder[i] = read();
+        postOrder = new int[n];
+        for (int i = 0; i < n; i++)
+            postOrder[i] = read();
+        getPreOrder(0, n - 1, 0, n - 1);
+        System.out.println(sb);
+    }
+
+    private static void getPreOrder(int il, int ir, int pl, int pr) {
+        if (il > ir || pl > pr)
+            return;
+        int root = postOrder[pr];
+        sb.append(root).append(" ");
+        int rootIdx = -1;
+        for (int i = il; i <= ir; i++)
+            if (inOrder[i] == root) {
+                rootIdx = i;
+                break;
+            }
+        getPreOrder(il, rootIdx - 1, pl, pl + rootIdx - il - 1);
+        getPreOrder(rootIdx + 1, ir, pl + rootIdx - il, pr - 1);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
