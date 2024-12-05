66e0684c6730d0167ace1ffbc6534bcef72124fa
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-05 15:08:09 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 13.08 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -3,62 +3,49 @@ class Solution {
         int answer = 0;
         int now = h1 * 3600 + m1 * 60 + s1;
         int end = h2 * 3600 + m2 * 60 + s2;
-        if (now == 0 || now == 43200) {
+        if (now == 0 || now == 43200)
             answer++;
-        }
         while (now < end) {
-            double[] nowAngle = stoA(now);
-            double[] nextAngle = stoA(now + 1);
-            boolean hourMatch = hourChk(nowAngle, nextAngle);
-            boolean minMatch = minChk(nowAngle, nextAngle);
-            if (hourMatch) {
+            double[] nowAngles = calculateAndgles(now);
+            double[] nextAngles = calculateAndgles(now + 1);
+            boolean isHourMatched = chkHour(nowAngles, nextAngles);
+            boolean isMinMatched = chkMin(nowAngles, nextAngles);
+            if (isHourMatched)
                 answer++;
-            }
-            if (minMatch) {
+            if (isMinMatched)
                 answer++;
-            }
-            if (hourMatch && minMatch) {
-                if (Double.compare(nextAngle[0], nextAngle[1]) == 0) {
+            if (isHourMatched && isMinMatched)
+                if (nextAngles[0] == nextAngles[1])
                     answer--;
-                }
-            }
             now++;
         }
         return answer;
     }
-    
-    public double[] stoA(int sec) {
+
+    private double[] calculateAndgles(int time) {
         double[] angles = new double[3];
-        int h = sec / 3600;
-        int m = sec % 3600 / 60;
-        int s = sec % 3600 % 60;
-        angles[0] = (h % 12) * 30d + m * 1 / 2d + s * 1 / 120d;
-        angles[1] = m * 6d + s * 1 / 10d;
-        angles[2] = s * 6d;
+        double h = time / 3600, m = time % 3600 / 60, s = time % 3600 % 60;
+        angles[0] = (h % 12) * (360d / 12) + m * (360d / 12 / 60) + s * (360d / 12 / 3600);
+        angles[1] = m * (360d / 60) + s * (360d / 60 / 60);
+        angles[2] = s * (360d / 60);
         return angles;
     }
-    
-    public boolean hourChk(double[] now, double[] next) {
-        double hA = now[0], sA = now[2];
-        double nhA = next[0], nsA = next[2];
-        if (Double.compare(hA, sA) > 0 && Double.compare(nhA, nsA) <= 0) {
+
+    private boolean chkHour(double[] now, double[] next) {
+        double nowSec = now[2], nextSec = next[2];
+        if (nowSec < now[0] && next[0] <= nextSec)
             return true;
-        }
-        if (Double.compare(sA, 354d) == 0 && Double.compare(hA, 354d) > 0) {
+        if (nowSec == 354d && 354d < now[0])
             return true;
-        }
         return false;
     }
 
-    public boolean minChk(double[] now, double[] next) {
-        double mA = now[1], sA = now[2];
-        double nmA = next[1], nsA = next[2];
-        if (Double.compare(mA, sA) > 0 && Double.compare(nmA, nsA) <= 0) {
+    private boolean chkMin(double[] now, double[] next) {
+        double nowSec = now[2], nextSec = next[2];
+        if (nowSec < now[1] && next[1] <= nextSec)
             return true;
-        }
-        if (Double.compare(sA, 354d) == 0 && Double.compare(mA, 354d) > 0) {
+        if (nowSec == 354d && 354d < now[1])
             return true;
-        }
         return false;
     }
 }
