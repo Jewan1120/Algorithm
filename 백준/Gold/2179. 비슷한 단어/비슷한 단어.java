a74a8c07cb5b4de13633ab8b2fc87bd6957245eb
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-17 18:05:22 +0900

    [Gold III] Title: 비슷한 단어, Time: 1364 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = br.readLine();
+        int maxlen = 0;
+        String str1 = "", str2 = "";
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++) {
+                int len = getPrefixLen(arr[i], arr[j]);
+                if (len > maxlen) {
+                    maxlen = len;
+                    str1 = arr[i];
+                    str2 = arr[j];
+                }
+            }
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+
+    private static int getPrefixLen(String str1, String str2) {
+        int len = 0;
+        int limit = Math.min(str1.length(), str2.length());
+        while (len < limit && str1.charAt(len) == str2.charAt(len))
+            len++;
+        return len;
+    }
+}
