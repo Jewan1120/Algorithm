0c4a90b9d4b97a681c60492c91abc16fd14042e0
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2024-12-19 16:03:50 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 476 ms, Memory: 47284 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,22 +1,30 @@
 public class Main {
+
     static StringBuilder sb = new StringBuilder();
-    
+
     public static void main(String[] args) throws Exception {
-        int c, n = System.in.read() & 15;
-        while((c = System.in.read()) >= 48) {
-            n = (n << 3) + (n << 1) + (c & 15);
-        }
+        int n = read();
         sb.append((int) Math.pow(2, n) - 1).append("\n");
-        hanoi(n, 1 , 2, 3);
+        recursive(n, 1, 2, 3);
         System.out.println(sb);
     }
-    private static void hanoi(int n, int s, int m, int e) {
-        if (n == 1) {
+
+    private static void recursive(int depth, int s, int m, int e) {
+        if (depth == 1) {
             sb.append(s).append(" ").append(e).append("\n");
             return;
         }
-        hanoi(n - 1, s, e, m);
+        recursive(depth - 1, s, e, m);
         sb.append(s).append(" ").append(e).append("\n");
-        hanoi(n - 1, m, s, e);
+        recursive(depth - 1, m, s, e);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
     }
 }
