2d98dd4599d50c242305b91b2956a9ee4f45577e
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-01 00:42:43 +0900

    [D1] Title: 중간값 찾기, Time: 104 ms, Memory: 19,072 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        System.out.println(arr[n / 2]);
+    }
+}
