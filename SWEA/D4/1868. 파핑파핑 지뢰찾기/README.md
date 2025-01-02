c5fc760ca3466abc1e1ae360bff78ff406393da8
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-02 23:23:00 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 181 ms, Memory: 35,012 KB -BaekjoonHub

---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/README.md
@@ -0,0 +1,15 @@
+# [D4] 파핑파핑 지뢰찾기 - 1868 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LwsHaD1MDFAXc) 
+
+### 성능 요약
+
+메모리: 35,012 KB, 시간: 181 ms, 코드길이: 2,602 Bytes
+
+### 제출 일자
+
+2025-01-02 23:22
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+
+    static int n;
+    static boolean[][] board, visited;
+    static int[] dy = { 1, -1, 0, 0, -1, -1, 1, 1 };
+    static int[] dx = { 0, 0, 1, -1, -1, 1, 1, -1 };
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int tt = 1; tt <= t; tt++) {
+            n = Integer.parseInt(br.readLine());
+            board = new boolean[n][n];
+            visited = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                char[] line = br.readLine().toCharArray();
+                for (int j = 0; j < n; j++) {
+                    if (line[j] == '*')
+                        checkMine(i, j);
+                }
+            }
+            int totalCnt = simulate();
+            sb.append("#").append(tt).append(" ").append(totalCnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void checkMine(int y, int x) {
+        board[y][x] = true;
+        visited[y][x] = true;
+        for (int i = 0; i < 8; i++) {
+            int ny = y + dy[i], nx = x + dx[i];
+            if (isValid(ny, nx))
+                board[ny][nx] = true;
+        }
+    }
+
+    private static int simulate() {
+        int totalCnt = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (!visited[i][j] && !board[i][j]) {
+                    bfs(i, j);
+                    totalCnt++;
+                }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (!visited[i][j])
+                    totalCnt++;
+        return totalCnt;
+    }
+
+    private static void bfs(int sy, int sx) {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[] { sy, sx });
+        visited[sy][sx] = true;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            int y = cur[0], x = cur[1];
+            for (int i = 0; i < 8; i++) {
+                int ny = y + dy[i], nx = x + dx[i];
+                if (isValid(ny, nx) && !visited[ny][nx]) {
+                    if (!board[ny][nx])
+                        dq.offer(new int[] { ny, nx });
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+    }
+
+    private static boolean isValid(int y, int x) {
+        return 0 <= y && y < n && 0 <= x && x < n;
+    }
+}
