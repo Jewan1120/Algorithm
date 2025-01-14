57960667bce17251c9559b5b2583d4b3bee569bc
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-14 21:19:46 +0900

    [Gold IV] Title: PPAP, Time: 260 ms, Memory: 24424 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -1,41 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        Stack<Character> stk = new Stack<>();
-        int pCnt = 0;
-        boolean flg = false;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == 'P') {
-                if (flg) {
-                    pCnt--;
-                    stk.pop();
-                    stk.pop();
-                } else {
-                    pCnt++;
-                    stk.push(c);
-                }
-                flg = false;
-            } else {
-                if (!flg) {
-                    if (pCnt >= 2) {
-                        flg = true;
-                    } else {
-                        pCnt = 0;
-                    }
-                } else {
-                    flg = false;
-                    pCnt = 0;
-                }
-                stk.push(c);
-            }
+        if (str.charAt(str.length() - 1) == 'A') {
+            System.out.println("NP");
+            return;
         }
-        System.out.println(pCnt == 1 && stk.size() == 1 ? "PPAP" : "NP");
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < str.length() - 1; i++) {
+            char c = str.charAt(i);
+            if (c == 'A') {
+                if (!(str.charAt(i + 1) == 'P' && dq.size() >= 2)) {
+                    System.out.println("NP");
+                    return;
+                } else {
+                    dq.poll();
+                    dq.poll();
+                }
+            } else
+                dq.offerLast(c);
+        }
+        System.out.println(dq.isEmpty() ? "PPAP" : "NP");
     }
 }
