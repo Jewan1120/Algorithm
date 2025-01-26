391a133a06a70a9ec99085a0022a61abfa441b8b
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-26 14:49:35 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -8,15 +8,12 @@ public class Main {
         for (int i = 1; i < n + 1; i++)
             arr[i] = read();
         Arrays.sort(arr);
-        int[][] dp = new int[n + 1][k + 1];
-        dp[0][0] = 1;
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
         for (int i = 1; i < n + 1; i++)
-            for (int j = 0; j < k + 1; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= arr[i])
-                    dp[i][j] += dp[i][j - arr[i]];
-            }
-        System.out.println(dp[n][k]);
+            for (int j = arr[i]; j < k + 1; j++)
+                dp[j] += dp[j - arr[i]];
+        System.out.println(dp[k]);
     }
 
     private static int read() throws Exception {
