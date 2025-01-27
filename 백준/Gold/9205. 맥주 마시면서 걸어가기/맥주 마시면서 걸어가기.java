88d80998eb954f1d99f5dc19681bff3766bd8341
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-27 14:06:18 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 120 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,60 +1,69 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n;
+    static int[][] distance;
+    static ArrayList<ArrayList<Integer>> graph;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int t = read();
         StringBuilder sb = new StringBuilder();
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            StringTokenizer st;
-            int[][] dist = new int[n + 2][2];
-            for (int i = 0; i < n + 2; i++) {
-                st = new StringTokenizer(br.readLine());
-                dist[i][0] = Integer.parseInt(st.nextToken());
-                dist[i][1] = Integer.parseInt(st.nextToken());
-            }
-            boolean[][] possible = new boolean[n + 2][n + 2];
+            n = read();
+            distance = new int[n + 2][];
             for (int i = 0; i < n + 2; i++)
-                for (int j = 0; j < n + 2; j++) {
-                    if (canGo(dist[i], dist[j]))
-                        possible[i][j] = true;
-                }
-            if (isReachable(possible)) {
-                sb.append("happy");
-            } else {
-                sb.append("sad");
-            }
-            sb.append("\n");
+                distance[i] = new int[] { read(), read() };
+            graph = new ArrayList<>();
+            for (int i = 0; i < n + 2; i++)
+                graph.add(new ArrayList<>());
+            for (int i = 0; i < n + 1; i++)
+                for (int j = i + 1; j < n + 2; j++)
+                    if (isMovable(distance[i], distance[j])) {
+                        graph.get(i).add(j);
+                        graph.get(j).add(i);
+                    }
+            if (isPossible())
+                sb.append("happy\n");
+            else
+                sb.append("sad\n");
         }
         System.out.println(sb);
     }
 
-    private static boolean canGo(int[] A, int[] B) {
-        return Math.abs(A[0] - B[0]) + Math.abs(A[1] - B[1]) <= 1000;
-    }
-
-    private static boolean isReachable(boolean[][] possible) {
-        int len = possible.length;
+    private static boolean isPossible() {
         Deque<Integer> dq = new ArrayDeque<>();
-        boolean[] visited = new boolean[len];
+        boolean[] visited = new boolean[n + 2];
         dq.offer(0);
         visited[0] = true;
         while (!dq.isEmpty()) {
             int cur = dq.poll();
-            if (cur == len - 1) 
+            if (cur == n + 1)
                 return true;
-            for (int i = 0; i < len; i++) 
-                if (possible[cur][i] && !visited[i]) {
-                    dq.offer(i);
-                    visited[i] = true;
+            for (int next : graph.get(cur))
+                if (!visited[next]) {
+                    dq.offer(next);
+                    visited[next] = true;
                 }
         }
         return false;
+    }
+
+    private static boolean isMovable(int[] A, int[] B) {
+        return Math.abs(A[0] - B[0]) + Math.abs(A[1] - B[1]) <= 1_000;
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)
+            n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return m ? ~n + 1 : n;
     }
 }
