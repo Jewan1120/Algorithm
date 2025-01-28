23aaf288645933921b52396c9d0cec40259b1139
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-28 19:38:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,27 +1,31 @@
 public class Main {
-    static int MOD = 1_000_000_000;
+
     public static void main(String[] args) throws Exception {
+        int MOD = 1_000_000_000;
         int n = read();
         long[][] dp = new long[n + 1][10];
-        for (int i = 1; i <= 9; i++) { // DP 초기화
+        for (int i = 1; i < 10; i++)
             dp[1][i] = 1;
-        }
-        for (int i = 2; i <= n; i++) { // 1 ~ n번째 계단까지
-            for (int j = 0; j <= 9; j++) { // 끝자리가 j로 끝나는 수의 개수
-                if (j > 0) dp[i][j] += dp[i - 1][j - 1];
-                if (j < 9) dp[i][j] += dp[i - 1][j + 1];
-                dp[i][j] %= MOD;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j != 0)
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+                if (j != 9)
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % MOD;
             }
         }
         long sum = 0;
-        for (long i : dp[n])
-            sum = (sum + i) % MOD;
+        for (int i = 0; i < 10; i++)
+            sum = (sum + dp[n][i]) % MOD;
         System.out.println(sum);
     }
+
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
