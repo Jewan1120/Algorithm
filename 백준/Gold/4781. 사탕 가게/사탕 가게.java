dddcfa4963541805fcdb28e73b66a4018d44eb6b
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-30 19:06:00 +0900

    [Gold IV] Title: 사탕 가게, Time: 532 ms, Memory: 31796 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -4,27 +4,28 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n, c, m, p;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = (int) (Double.parseDouble(st.nextToken()) * 100);
-            if (n == 0 && m == 0)
-                break;
-            int[][] candies = new int[n][2];
-            for (int i = 0; i < n; i++) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
+            StringBuilder sb = new StringBuilder();
+            while (true) {
                 st = new StringTokenizer(br.readLine());
-                candies[i][0] = Integer.parseInt(st.nextToken());
-                candies[i][1] = (int) Math.round(Double.parseDouble(st.nextToken()) * 100);
+                n = Integer.parseInt(st.nextToken());
+                m = (int) Math.round(Double.parseDouble(st.nextToken()) * 100);
+                if (n == 0)
+                    break;
+                int[] dp = new int[m + 1];
+                for (int i = 0; i < n; i++) {
+                    st = new StringTokenizer(br.readLine());
+                    c = Integer.parseInt(st.nextToken());
+                    p = (int) Math.round(Double.parseDouble(st.nextToken()) * 100);
+                    for (int j = p; j < m + 1; j++)
+                        dp[j] = Math.max(dp[j], dp[j - p] + c);
+                }
+                sb.append(dp[m]).append("\n");
             }
-            int[] dp = new int[m + 1];
-            for (int[] candy : candies)
-                for (int i = candy[1]; i < m + 1; i++)
-                    dp[i] = Math.max(dp[i], dp[i - candy[1]] + candy[0]);
-            sb.append(dp[m]).append("\n");
+            System.out.println(sb);
         }
-        System.out.println(sb);
-    }
 }
