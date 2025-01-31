1c60b40c3dcdad475d4eae3536fb9de3ff6c2070
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-01-31 09:16:10 +0900

    [level 3] Title: 섬 연결하기, Time: 1.06 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,41 +1,30 @@
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 class Solution {
-    
-    class Edge {
-        int to, weight;
-
-        Edge(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        ArrayList<ArrayList<Edge>> edges = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> edges = new ArrayList<>();
         for (int i = 0; i < n; i++)
             edges.add(new ArrayList<>());
         for (int[] cost : costs) {
-            edges.get(cost[0]).add(new Edge(cost[1], cost[2]));
-            edges.get(cost[1]).add(new Edge(cost[0], cost[2]));
+            int u = cost[0], v = cost[1], w = cost[2];
+            edges.get(u).add(new int[] { v, w });
+            edges.get(v).add(new int[] { u, w });
         }
-        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((o1, o2) -> o1[1] - o2[1]);
         boolean[] visited = new boolean[n];
-        pq.offer(new Edge(0, 0));
-        int cnt = 0;
-        while (!pq.isEmpty() && cnt < n) {
-            Edge cur = pq.poll();
-            if (visited[cur.to])
+        pq.offer(new int[] { 0, 0 });
+        int totalWeight = 0;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (visited[cur[0]])
                 continue;
-            visited[cur.to] = true;
-            cnt++;
-            answer += cur.weight;
-            for (Edge edge : edges.get(cur.to)) {
-                if (!visited[edge.to])
+            visited[cur[0]] = true;
+            totalWeight += cur[1];
+            for (int[] edge : edges.get(cur[0]))
+                if (!visited[edge[0]]) {
                     pq.offer(edge);
-            }
+                }
         }
-        return answer;
+        return totalWeight;
     }
 }
