363d046c4bfacdba489aa32b75c865033b4bd042
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-02 22:50:55 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,37 +1,28 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        for (; n > 0; n--) {
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            Deque<Character> dq = new ArrayDeque<>();
             String str = br.readLine();
-            Stack<Character> stk = new Stack<>();
             for (char c : str.toCharArray()) {
-                if (stk.isEmpty()) {
-                    stk.push(c);
-                } else {
-                    if (stk.peek() == '(' && c == ')') {
-                        stk.pop();
-                    } else {
-                        stk.push(c);
-                    }
+                if (dq.isEmpty()) {
+                    dq.offerLast(c);
+                    continue;
                 }
+                if (dq.peekLast() == '(' && c == ')')
+                    dq.pollLast();
+                else
+                    dq.offerLast(c);
             }
-            if (stk.size() == 0) {
-                bw.write("YES\n");
-            } else {
-                bw.write("NO\n");
-            }
+            sb.append(dq.isEmpty() ? "YES" : "NO").append("\n");
         }
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
