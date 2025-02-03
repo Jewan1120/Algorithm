81cd511e23d3bc400a67d2858a621ba9996e4fa0
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-03 10:03:06 +0900

    [Gold V] Title: 입국심사, Time: 188 ms, Memory: 15168 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    static long n, m;
+    static long[] arr;
+
+    public static void main(String[] args) throws Exception {
+        n = read();
+        m = read();
+        arr = new long[(int) n];
+        long maxTime = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = read();
+            maxTime = Math.max(maxTime, arr[i]);
+        }
+        long l = 1, r = maxTime * m;
+        while (l <= r) {
+            long m = (l + r) / 2;
+            if (isPossible(m))
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        System.out.println(l);
+    }
+
+    private static boolean isPossible(long target) {
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += target / arr[i];
+            if(cnt >= m)
+                return true;
+        }
+        return cnt >= m;
+    }
+
+    private static long read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
