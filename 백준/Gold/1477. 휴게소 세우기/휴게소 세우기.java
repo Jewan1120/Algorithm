81e2b27a91b3f48aa39d3aab8ccd4144f2e08798
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-04 15:24:44 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -2,17 +2,22 @@ import java.util.Arrays;
 
 public class Main {
 
+    static int n, m, l;
+    static int[] arr;
+
     public static void main(String[] args) throws Exception {
-        int n = read(), m = read(), l = read();
-        int[] arr = new int[n + 2];
-        for (int i = 1; i <= n; i++)
+        n = read();
+        m = read();
+        l = read();
+        arr = new int[n + 1];
+        for (int i = 0; i < n; i++)
             arr[i] = read();
-        arr[n + 1] = l;
+        arr[n] = l;
         Arrays.sort(arr);
         int left = 1, right = l;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (isPossible(arr, m, mid)) {
+            if (isPossible(mid)) {
                 right = mid - 1;
             } else
                 left = mid + 1;
@@ -20,10 +25,13 @@ public class Main {
         System.out.println(left);
     }
 
-    private static boolean isPossible(int[] arr, int m, int target) {
+    private static boolean isPossible(int target) {
         int cnt = 0;
-        for (int i = 1; i < arr.length; i++)
-            cnt += (arr[i] - arr[i - 1] - 1) / target;
+        int cur = 0;
+        for (int i = 0; i < n + 1; i++) {
+            cnt += (arr[i] - cur - 1) / target;
+            cur = arr[i];
+        }
         return cnt <= m;
     }
 
@@ -31,6 +39,8 @@ public class Main {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
