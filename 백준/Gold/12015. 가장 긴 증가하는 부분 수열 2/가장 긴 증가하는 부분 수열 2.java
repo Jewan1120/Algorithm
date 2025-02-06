ada269c3b55a32a4ebdba43fb6b0f82f0acb0e0f
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-06 14:59:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 320 ms, Memory: 22384 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,26 +1,31 @@
 import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
         int n = read();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++)
-             arr[i] = read();
+            arr[i] = read();
         int[] lis = new int[n];
         int len = 0;
-        for (int num : arr) {
-            int idx = Arrays.binarySearch(lis, 0, len, num);
-            if (idx < 0)
-                idx = -(idx + 1);
-            lis[idx] = num;
-            if (idx == len)
+        for (int i = 0; i < n; i++) {
+            int pos = Arrays.binarySearch(lis, 0, len, arr[i]);
+            if (pos < 0)
+                pos = ~pos;
+            lis[pos] = arr[i];
+            if (pos == len)
                 len++;
         }
         System.out.println(len);
     }
+
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
