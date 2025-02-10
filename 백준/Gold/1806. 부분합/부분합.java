356adec7a5649a38d506a80f552be307610d8419
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-10 16:04:07 +0900

    [Gold IV] Title: 부분합, Time: 124 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,25 +1,28 @@
 public class Main {
 
+    static int n, s;
+    static int[] arr;
+
     public static void main(String[] args) throws Exception {
-        int n = read(), s = read();
-        int minLen = Integer.MAX_VALUE;
-        int[] arr = new int[n + 1];
+        n = read();
+        s = read();
+        arr = new int[n + 1];
         for (int i = 0; i < n; i++)
             arr[i] = read();
-        int l = 0, r = 0;
-        int sum = 0;
-        while (l <= n && r <= n) {
-            if (sum >= s) {
+        int l = 0, r = 0, sum = 0;
+        int minLen = Integer.MAX_VALUE;
+        while (r < n + 1) {
+            if (sum < s) {
+                sum += arr[r++];
+            } else {
                 minLen = Math.min(minLen, r - l);
                 sum -= arr[l++];
-            } else {
-                sum += arr[r++];
             }
         }
         System.out.println(minLen == Integer.MAX_VALUE ? 0 : minLen);
     }
 
-    public static int read() throws Exception {
+    private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
