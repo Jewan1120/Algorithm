f1c365c8d20c514e5e24d47954ee500d180c5455
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-10 13:18:45 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = read();
+        Arrays.sort(arr);
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            int l = 0, r = n - 1;
+            if (r == i)
+                r--;
+            if (l == i)
+                l++;
+            while (l < r) {
+                if (arr[l] + arr[r] == arr[i]) {
+                    total++;
+                    break;
+                } else if (arr[l] + arr[r] > arr[i]) {
+                    r--;
+                    if (r == i)
+                        r--;
+                } else {
+                    l++;
+                    if (l == i)
+                        l++;
+                }
+            }
+        }
+        System.out.println(total);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)
+            n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return m ? ~n + 1 : n;
+    }
+}
