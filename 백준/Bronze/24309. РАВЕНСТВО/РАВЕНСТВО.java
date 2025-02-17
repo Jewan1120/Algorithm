36d75813ccbc8a224e1a55def1f2db4a6afafef6
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-17 21:31:21 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        BigInteger c = new BigInteger(br.readLine());
+        System.out.println(b.subtract(c).divide(a));
+    }
+}
