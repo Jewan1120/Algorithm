2e2d439f9f97af73b1659d8fefb22020f4e0820c
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-17 21:03:35 +0900

    [Gold IV] Title: 트리, Time: 148 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -2,46 +2,49 @@ import java.util.HashSet;
 
 public class Main {
 
+    static int n, m;
     static int[] parent;
+    static boolean[] isCycle;
 
     public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
         int idx = 0;
         while (true) {
-            int n = read(), m = read();
+            n = read();
+            m = read();
             if (n == 0 && m == 0)
                 break;
             parent = new int[n + 1];
+            isCycle = new boolean[n + 1];
             for (int i = 0; i < n + 1; i++)
                 parent[i] = i;
-            boolean[] isCycle = new boolean[n + 1];
             for (int i = 0; i < m; i++) {
-                int x = read(), y = read();
-                int rootX = find(x), rootY = find(y);
-                if (rootX == rootY)
-                    isCycle[rootX] = true;
-                parent[rootX] = rootY;
-                isCycle[rootY] |= isCycle[rootX];
+                int u = read(), v = read();
+                int rootU = find(u), rootV = find(v);
+                if (rootU == rootV)
+                    isCycle[rootU] = true;
+                parent[rootU] = rootV;
+                isCycle[rootV] |= isCycle[rootU];
             }
-            HashSet<Integer> roots = new HashSet<>();
+            HashSet<Integer> hs = new HashSet<>();
             for (int i = 1; i < n + 1; i++) {
                 int root = find(i);
                 if (!isCycle[root])
-                    roots.add(root);
+                    hs.add(root);
             }
             sb.append("Case ").append(++idx).append(": ");
-            if (roots.size() == 0)
+            if (hs.size() == 0)
                 sb.append("No trees.\n");
-            else if (roots.size() == 1)
+            else if (hs.size() == 1)
                 sb.append("There is one tree.\n");
             else
-                sb.append("A forest of ").append(roots.size()).append(" trees.\n");
+                sb.append("A forest of ").append(hs.size()).append(" trees.\n");
         }
         System.out.println(sb);
     }
 
     private static int find(int x) {
-        if (x == parent[x])
+        if (parent[x] == x)
             return parent[x];
         return parent[x] = find(parent[x]);
     }
