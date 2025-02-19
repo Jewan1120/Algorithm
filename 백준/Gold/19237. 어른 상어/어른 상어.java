5db6050d5b75e481075920e8ed41504797940110
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-19 14:50:30 +0900

    [Gold II] Title: 어른 상어, Time: 152 ms, Memory: 16248 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.java
+++ 백준/Gold/19237. 어른 상어/어른 상어.java
@@ -1,18 +1,15 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
 
 public class Main {
 
     static class Shark {
-        int num, y, x, dir;
+        int num, idx, dir;
         int[][] priority;
 
-        Shark(int num, int y, int x, int dir, int[][] priority) {
+        Shark(int num, int idx, int dir, int[][] priority) {
             this.num = num;
-            this.y = y;
-            this.x = x;
+            this.idx = idx;
             this.dir = dir;
             this.priority = priority;
         }
@@ -23,19 +20,19 @@ public class Main {
     }
 
     static int n, m, k;
-    static int[][][] scentBoard;
-    static boolean[][] visited;
+    static int[] scentBoard, remainBoard, move;
+    static boolean[] visited;
     static Deque<Shark> sharks = new ArrayDeque<>();
     static Deque<int[]> scentDeque = new ArrayDeque<>();
-
-    static int[] dy = { 0, -1, 1, 0, 0 }, dx = { 0, 0, 0, -1, 1 };
 
     public static void main(String[] args) throws Exception {
         n = read();
         m = read();
         k = read();
-        scentBoard = new int[2][n][n];
-        visited = new boolean[n][n];
+        move = new int[] { 0, -n, n, -1, 1 };
+        scentBoard = new int[n * n];
+        remainBoard = new int[n * n];
+        visited = new boolean[n * n];
         init();
         int time = 0;
         while (time <= 1000) {
@@ -44,21 +41,19 @@ public class Main {
                 System.out.println(time);
                 return;
             }
-            for (int i = 0; i < n; i++)
-                Arrays.fill(visited[i], false);
+            visited = new boolean[n * n];
             while (size-- > 0) {
                 Shark shark = sharks.poll();
-                int num = shark.num, y = shark.y, x = shark.x;
+                int num = shark.num, idx = shark.idx;
                 int nextDir = getNextDir(shark);
-                int ny = y + dy[nextDir], nx = x + dx[nextDir];
-                if (visited[ny][nx])
+                int next = idx + move[nextDir];
+                if (visited[next])
                     continue;
-                visited[ny][nx] = true;
-                shark.y = ny;
-                shark.x = nx;
+                visited[next] = true;
+                shark.idx = next;
                 shark.dir = nextDir;
                 sharks.offer(shark);
-                scentDeque.offer(new int[] { ny, nx, num });
+                scentDeque.offer(new int[] { next, num });
             }
             removeScent();
             putScent();
@@ -68,26 +63,25 @@ public class Main {
     }
 
     private static void init() throws Exception {
-        int[][] tempSharks = new int[m + 1][];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                int num = read();
-                if (num != 0) {
-                    tempSharks[num] = new int[] { i, j };
-                    scentBoard[0][i][j] = num;
-                    scentBoard[1][i][j] = k;
-                }
+        int[] tempSharks = new int[m + 1];
+        for (int i = 0; i < n * n; i++) {
+            int num = read();
+            if (num != 0) {
+                tempSharks[num] = i;
+                scentBoard[i] = num;
+                remainBoard[i] = k;
             }
+        }
         int[] tempDir = new int[m + 1];
         for (int i = 1; i < m + 1; i++)
             tempDir[i] = read();
         for (int i = 1; i < m + 1; i++) {
-            int y = tempSharks[i][0], x = tempSharks[i][1], dir = tempDir[i];
+            int idx = tempSharks[i], dir = tempDir[i];
             int[][] priority = new int[5][5];
             for (int j = 1; j < 5; j++)
                 for (int k = 1; k < 5; k++)
                     priority[j][k] = read();
-            Shark shark = new Shark(i, y, x, dir, priority);
+            Shark shark = new Shark(i, idx, dir, priority);
             sharks.offer(shark);
         }
     }
@@ -97,11 +91,11 @@ public class Main {
         int myScent = 0;
         for (int i = 1; i < 5; i++) {
             int dir = priority[i];
-            int ny = shark.y + dy[dir], nx = shark.x + dx[dir];
-            if (isValid(ny, nx)) {
-                if (scentBoard[0][ny][nx] == 0)
+            int next = shark.idx + move[dir];
+            if (isValid(shark.idx, next)) {
+                if (scentBoard[next] == 0)
                     return dir;
-                else if (scentBoard[0][ny][nx] == shark.num && myScent == 0)
+                else if (scentBoard[next] == shark.num && myScent == 0)
                     myScent = dir;
             }
         }
@@ -109,23 +103,28 @@ public class Main {
     }
 
     private static void removeScent() {
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (scentBoard[0][i][j] != 0 && --scentBoard[1][i][j] == 0)
-                    scentBoard[0][i][j] = 0;
+        for (int i = 0; i < n * n; i++)
+            if (scentBoard[i] != 0 && --remainBoard[i] == 0)
+                scentBoard[i] = 0;
     }
 
     private static void putScent() {
         while (!scentDeque.isEmpty()) {
             int[] scent = scentDeque.poll();
-            int y = scent[0], x = scent[1], num = scent[2];
-            scentBoard[0][y][x] = num;
-            scentBoard[1][y][x] = k;
+            int idx = scent[0], num = scent[1];
+            scentBoard[idx] = num;
+            remainBoard[idx] = k;
         }
     }
 
-    private static boolean isValid(int y, int x) {
-        return 0 <= y && y < n && 0 <= x && x < n;
+    private static boolean isValid(int cur, int next) {
+        if (next < 0 || next >= n * n)
+            return false;
+        int curRow = cur / n;
+        int nextRow = next / n;
+        if (Math.abs(cur - next) == 1 && curRow != nextRow)
+            return false;
+        return true;
     }
 
     private static int read() throws Exception {
