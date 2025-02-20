3246a3cbe9a0b87f52e5a1de13e95082a7fffbf2
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-20 14:37:04 +0900

    [D2] Title: 파리 퇴치, Time: 76 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/README.md
@@ -0,0 +1,15 @@
+# [D2] 파리 퇴치 - 2001 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq) 
+
+### 성능 요약
+
+메모리: 25,472 KB, 시간: 76 ms, 코드길이: 1,635 Bytes
+
+### 제출 일자
+
+2025-02-20 14:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static int n, m;
+    static int[][] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            board = new int[n + 1][n + 1];
+            for (int i = 1; i < n + 1; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j < n + 1; j++)
+                    board[i][j] = Integer.parseInt(st.nextToken());
+            }
+            for (int i = 1; i < n + 1; i++)
+                for (int j = 1; j < n + 1; j++)
+                    board[i][j] += board[i][j - 1];
+            for (int i = 1; i < n + 1; i++)
+                for (int j = 1; j < n + 1; j++)
+                    board[j][i] += board[j - 1][i];
+            int maxValue = 0;
+            for (int i = m; i < n + 1; i++)
+                for (int j = m; j < n + 1; j++) {
+                    int value = board[i][j] - board[i - m][j] - board[i][j - m] + board[i - m][j - m];
+                    maxValue = Math.max(maxValue, value);
+                }
+            sb.append("#").append(tc).append(" ").append(maxValue).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
