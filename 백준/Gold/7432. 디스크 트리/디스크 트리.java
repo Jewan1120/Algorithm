6f2134a52d3205743a774713b8d15277fef3dce0
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-22 10:43:20 +0900

    [Gold III] Title: 디스크 트리, Time: 200 ms, Memory: 22736 KB -BaekjoonHub

--- 백준/Gold/7432. 디스크 트리/디스크 트리.java
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class Main {
 
     static class Node {
         TreeMap<String, Node> next = new TreeMap<>();
+        String value;
+
+        Node(String value) {
+            this.value = value;
+        }
 
         void insert(String dir) {
             String[] strArr = dir.split("\\\\");
             Node cur = this;
             for (String str : strArr) {
-                cur.next.putIfAbsent(str, new Node());
+                cur.next.putIfAbsent(str, new Node(str));
                 cur = cur.next.get(str);
             }
         }
@@ -23,9 +27,9 @@ public class Main {
         }
 
         void print(int depth, Node cur, StringBuilder sb) {
-            for (Map.Entry<String, Node> entry : cur.next.entrySet()) {
-                sb.append(" ".repeat(depth)).append(entry.getKey()).append("\n");
-                print(depth + 1, entry.getValue(), sb);
+            for (Node node : cur.next.values()) {
+                sb.append(" ".repeat(depth)).append(node.value).append("\n");
+                print(depth + 1, node, sb);
             }
         }
     }
@@ -33,7 +37,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Node root = new Node();
+        Node root = new Node("");
         while (n-- > 0)
             root.insert(br.readLine());
         StringBuilder sb = new StringBuilder();
