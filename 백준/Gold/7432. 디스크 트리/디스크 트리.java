691875ffe3b20548a65989e10760191c4234483e
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-22 10:31:48 +0900

    [Gold III] Title: 디스크 트리, Time: 268 ms, Memory: 27228 KB -BaekjoonHub

--- 백준/Gold/7432. 디스크 트리/디스크 트리.java
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.java
@@ -1,79 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 public class Main {
 
     static class Node {
-        Node[] next;
-        boolean isEnd;
+        TreeMap<String, Node> next = new TreeMap<>();
 
-        Node() {
-            next = new Node[hm.size()];
-            isEnd = false;
-        }
-    }
-
-    static class Trie {
-        Node root;
-
-        Trie() {
-            root = new Node();
-        }
-
-        void insert(String[] strArr) {
-            Node cur = root;
+        void insert(String dir) {
+            String[] strArr = dir.split("\\\\");
+            Node cur = this;
             for (String str : strArr) {
-                int idx = hm.get(str);
-                if (cur.next[idx] == null)
-                    cur.next[idx] = new Node();
-                cur = cur.next[idx];
+                cur.next.putIfAbsent(str, new Node());
+                cur = cur.next.get(str);
             }
-            cur.isEnd = true;
         }
 
-        String print() {
-            print(root, 0);
-            return sb.toString();
+        void print() {
+            print(0, this);
         }
 
-        void print(Node node, int depth) {
-            for (int i = 0; i < hm.size(); i++) {
-                if (node.next[i] != null) {
-                    for (int j = 0; j < depth; j++)
-                        sb.append(" ");
-                    sb.append(convert.get(i)).append("\n");
-                    print(node.next[i], depth + 1);
-                }
+        void print(int depth, Node cur) {
+            for (String str : cur.next.keySet()) {
+                for (int i = 0; i < depth; i++)
+                    sb.append(" ");
+                sb.append(str).append("\n");
+                print(depth + 1, cur.next.get(str));
             }
         }
     }
 
-    static HashMap<String, Integer> hm = new HashMap<>();
-    static TreeSet<String> ts = new TreeSet<>();
-    static HashMap<Integer, String> convert = new HashMap<>();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int idx = 0;
-        String[][] strArr = new String[n][];
-        for (int i = 0; i < n; i++) {
-            strArr[i] = br.readLine().split("\\\\");
-            for (String str : strArr[i])
-                ts.add(str);
-        }
-        for (String str : ts) {
-            if (!hm.containsKey(str)) {
-                hm.put(str, idx);
-                convert.put(idx++, str);
-            }
-        }
-        Trie trie = new Trie();
-        for (int i = 0; i < n; i++)
-            trie.insert(strArr[i]);
-        System.out.println(trie.print());
+        Node root = new Node();
+        while (n-- > 0)
+            root.insert(br.readLine());
+        root.print();
+        System.out.println(sb);
     }
 }
