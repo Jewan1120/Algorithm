98573362685522eca7f191f591e543db71cc0589
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-25 18:50:07 +0900

    [Silver I] Title: 스타트와 링크, Time: 116 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,42 +1,46 @@
 public class Main {
-    static int n;
-    static int[][] status;
-    static boolean[] team;
-    static int min = Integer.MAX_VALUE;
+
+    static int n, minDiff = Integer.MAX_VALUE;
+    static int[][] board;
+    static int[] rowSum, colSum;
+
     public static void main(String[] args) throws Exception {
         n = read();
-        status = new int[n][n];
-        team = new boolean[n];
+        board = new int[n][n];
+        rowSum = new int[n];
+        colSum = new int[n];
+        int totalSum = 0;
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                status[i][j] = read();
-        match(0, 0);
-        System.out.println(min);
-    }
-    private static void match(int cnt, int idx) {
-        if (cnt == n / 2) {
-            int scoreA = 0, scoreB = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++){
-                    if (team[i] && team[j]) scoreA += status[i][j];
-                    else if (!team[i] && !team[j]) scoreB += status[i][j];
-                }
+            for (int j = 0; j < n; j++) {
+                board[i][j] = read();
+                totalSum += board[i][j];
+                rowSum[i] += board[i][j];
+                colSum[j] += board[i][j];
             }
-            min = Math.min(min, Math.abs(scoreA - scoreB));
+        recursive(1, 1, totalSum - rowSum[0] - colSum[0]);
+        System.out.println(minDiff);
+    }
+
+    private static void recursive(int depth, int cnt, int value) {
+        if (cnt > n / 2)
+            return;
+        if (cnt == n / 2) {
+            minDiff = Math.min(minDiff, Math.abs(value));
             return;
         }
-        for (int i = idx; i < n; i++) {
-            if (!team[i]) {
-                team[i] = true;
-                match(cnt + 1, i + 1);
-                team[i] = false;
-            }
-        }
+        if (depth == n)
+            return;
+        recursive(depth + 1, cnt, value);
+        recursive(depth + 1, cnt + 1, value - rowSum[depth] - colSum[depth]);
+
     }
+
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
         return n;
     }
 }
