0a8dad32a9d821e38eee8d69ff67fd29743138b8
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-25 20:07:31 +0900

    [Gold IV] Title: ⚾, Time: 376 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -0,0 +1,67 @@
+public class Main {
+
+    static int n, maxScore = Integer.MIN_VALUE;
+    static int[][] rounds;
+
+    static int[] orders = new int[9];
+    static boolean[] visited = { true, false, false, false, false, false, false, false, false };
+
+    public static void main(String[] args) throws Exception {
+        n = read();
+        rounds = new int[n][9];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 9; j++)
+                rounds[i][j] = read();
+        recursive(0);
+        System.out.println(maxScore);
+    }
+
+    private static void recursive(int depth) {
+        if (depth == 9) {
+            int score = simulate();
+            maxScore = Math.max(maxScore, score);
+            return;
+        }
+        if (depth == 3) {
+            recursive(depth + 1);
+            return;
+        }
+        for (int i = 1; i < 9; i++)
+            if (!visited[i]) {
+                visited[i] = true;
+                orders[depth] = i;
+                recursive(depth + 1);
+                visited[i] = false;
+            }
+    }
+
+    private static int simulate() {
+        int score = 0, index = 0;
+        for (int i = 0; i < n; i++) {
+            int[] round = rounds[i];
+            int result = 0, outCount = 0;
+            while (outCount < 3) {
+                int oper = round[orders[index]];
+                index = (index + 1) % 9;
+                if (oper == 0) {
+                    outCount++;
+                    continue;
+                }
+                result <<= oper;
+                result |= (1 << (oper - 1));
+                score += Integer.bitCount(result >> 3);
+                result &= 7;
+            }
+        }
+        return score;
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
