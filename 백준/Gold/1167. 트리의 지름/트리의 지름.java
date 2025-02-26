a84d23732177615244a79bd931934d6cfcd62a91
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-26 09:38:59 +0900

    [Gold II] Title: 트리의 지름, Time: 332 ms, Memory: 53452 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,53 +1,41 @@
 import java.util.ArrayList;
+
 public class Main {
-    
-    static class Edge {
-        int t;
-        int w;
 
-        public Edge(int t, int w) {
-            this.t = t;
-            this.w = w;
-        }
-    }
-
-    static ArrayList<ArrayList<Edge>> tree = new ArrayList<>();
+    static ArrayList<ArrayList<int[]>> edges = new ArrayList<>();
     static boolean[] visited;
-    static int mV;
-    static int mL;
+
+    static int maxV = 0, maxLen = 0;
 
     public static void main(String[] args) throws Exception {
-        int n = read();
-        for (int i = 0; i <= n; i++) {
-            tree.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            int u = read();
-            int v;
-            while ((v = read()) != -1) {
-                int w = read();
-                tree.get(u).add(new Edge(v, w));
-                tree.get(v).add(new Edge(u, w));
+        int v = read();
+        for (int i = 0; i < v + 1; i++)
+            edges.add(new ArrayList<>());
+        for (int i = 0; i < v; i++) {
+            int from = read();
+            int to, dist;
+            while ((to = read()) != -1) {
+                dist = read();
+                edges.get(from).add(new int[] { to, dist });
             }
         }
-        visited = new boolean[n + 1];
+        visited = new boolean[v + 1];
         dfs(1, 0);
-        visited = new boolean[n + 1];
-        dfs(mV, 0);
-        System.out.println(mL);
+        visited = new boolean[v + 1];
+        dfs(maxV, 0);
+        System.out.println(maxLen);
     }
 
-    private static void dfs(int s, int l) {
-        if (l > mL) {
-            mV = s;
-            mL = l;
+    private static void dfs(int cur, int len) {
+        if (len > maxLen) {
+            maxV = cur;
+            maxLen = len;
         }
-        visited[s] = true;
-        for (Edge edge : tree.get(s)) {
-            if (!visited[edge.t]) {
-                dfs(edge.t, l + edge.w);
+        visited[cur] = true;
+        for (int[] edge : edges.get(cur))
+            if (!visited[edge[0]]) {
+                dfs(edge[0], len + edge[1]);
             }
-        }
     }
 
     private static int read() throws Exception {
@@ -55,9 +43,8 @@ public class Main {
         boolean m = n == 13;
         if (m)
             n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
+        while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
-        }
         return m ? ~n + 1 : n;
     }
 }
