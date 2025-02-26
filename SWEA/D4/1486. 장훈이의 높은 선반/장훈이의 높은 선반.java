986dfd9aa628f406d9998d3be49b196369759e15
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-26 15:43:43 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 174 ms, Memory: 41,552 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,41 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Solution {
-
-    static int n, b;
-    static int[] arr;
-    static int min;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        for (int tt = 1; tt <= t; tt++) {
+        int n, b;
+        TreeSet<Integer> ts;
+        for (int tc = 1; tc <= t; tc++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
-            arr = new int[n];
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++)
-                arr[i] = Integer.parseInt(st.nextToken());
-            min = Integer.MAX_VALUE;
-            recursive(0, 0);
-            sb.append("#").append(tt).append(" ").append(min).append("\n");
+            ts = new TreeSet<>();
+            ts.add(0);
+            for (int i = 0; i < n; i++) {
+                int v = Integer.parseInt(st.nextToken());
+                for (int u : new ArrayList<>(ts))
+                    ts.add(u + v);
+            }
+            sb.append("#").append(tc).append(" ").append(ts.ceiling(b) - b).append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static void recursive(int depth, int sum) {
-        if (sum >= b) {
-            min = Math.min(min, sum - b);
-            return;
-        }
-        if(depth == n)
-            return;
-        recursive(depth + 1, sum + arr[depth]);
-        recursive(depth + 1, sum);
     }
 }
