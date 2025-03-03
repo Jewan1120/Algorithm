85de7182d100d8cb4eb41129a8d762e3d05b8571
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-03-03 13:17:26 +0900

    [Platinum V] Title: 어항 정리, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.java
+++ 백준/Platinum/23291. 어항 정리/어항 정리.java
@@ -132,8 +132,6 @@ public class Main {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) >= 48)
             n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13)
-            System.in.read();
         return n;
     }
 }
