ed56363ebe8e0574644ce18fc6b7ae99359273fb
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-03-06 10:37:35 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 181 ms, Memory: 35,012 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -2,35 +2,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-
+ 
 public class Solution {
-
+ 
     static int n;
     static boolean[][] board, visited;
     static int[] dy = { 1, -1, 0, 0, -1, -1, 1, 1 };
     static int[] dx = { 0, 0, 1, -1, -1, 1, 1, -1 };
-
+ 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int tt = 1; tt <= t; tt++) {
+        for (int tc = 1; tc <= t; tc++) {
             n = Integer.parseInt(br.readLine());
             board = new boolean[n][n];
             visited = new boolean[n][n];
             for (int i = 0; i < n; i++) {
                 char[] line = br.readLine().toCharArray();
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j < n; j++)
                     if (line[j] == '*')
                         checkMine(i, j);
-                }
             }
             int totalCnt = simulate();
-            sb.append("#").append(tt).append(" ").append(totalCnt).append("\n");
+            sb.append("#").append(tc).append(" ").append(totalCnt).append("\n");
         }
         System.out.println(sb);
     }
-
+ 
     private static void checkMine(int y, int x) {
         board[y][x] = true;
         visited[y][x] = true;
@@ -40,7 +39,7 @@ public class Solution {
                 board[ny][nx] = true;
         }
     }
-
+ 
     private static int simulate() {
         int totalCnt = 0;
         for (int i = 0; i < n; i++)
@@ -55,7 +54,7 @@ public class Solution {
                     totalCnt++;
         return totalCnt;
     }
-
+ 
     private static void bfs(int sy, int sx) {
         Deque<int[]> dq = new ArrayDeque<>();
         dq.offer(new int[] { sy, sx });
@@ -73,7 +72,7 @@ public class Solution {
             }
         }
     }
-
+ 
     private static boolean isValid(int y, int x) {
         return 0 <= y && y < n && 0 <= x && x < n;
     }
