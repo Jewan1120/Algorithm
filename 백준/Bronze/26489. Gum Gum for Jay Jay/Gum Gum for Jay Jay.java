e818f1174a16024024a5bb825ccda300ac640d57
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-03-13 19:51:16 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while (br.readLine() != null)
+            cnt++;
+        System.out.println(cnt);
+    }
+}
