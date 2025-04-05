a3193c2f0e5548b73478f5d282a4af8f61621e1f
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-04-05 22:21:04 +0900

    [Bronze V] Title: 라면 공식, Time: 204 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int x = sc.nextInt();
+			
+			System.out.println(a * (x - 1) + b);
+		}
+		sc.close();
+	}
+}
