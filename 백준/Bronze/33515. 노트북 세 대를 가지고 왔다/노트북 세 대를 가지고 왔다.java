fb6d83d38773c5c167423818436e4572207a9ffc
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-04-20 22:43:17 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(Math.min(read(), read()));
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
+}
