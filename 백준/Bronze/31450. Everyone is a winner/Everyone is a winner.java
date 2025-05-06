abed6e35ab878c837b7713d5dde2187ea1bfd4f8
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-05-06 13:43:08 +0900

    [Bronze V] Title: Everyone is a winner, Time: 100 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(read() % read() == 0 ? "Yes" : "No");
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return n;
+	}
+}
