d582c5ba99f4aeaaae476c5ba1197b21c24ef3a3
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-05-07 09:48:20 +0900

    [Bronze V] Title: Adding Trouble, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(read() + read() == read() ? "correct!" : "wrong!");
+	}
+
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean m = n == 13;
+		if (m)
+			n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13)
+			System.in.read();
+		return m ? ~n + 1 : n;
+	}
+}
