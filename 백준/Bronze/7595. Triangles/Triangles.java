ad85e0d1fd6c5bf605fefa01fe78db074ea27693
Jewan Baek <116615068+jewan100@users.noreply.github.com>
2025-06-13 23:03:56 +0900

    [Bronze IV] Title: Triangles, Time: 1776 ms, Memory: 42624 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		while(a != 0) {
+			for(int i = 1; i <= a; i++) {
+				for(int j = 1; j <= i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			a = sc.nextInt();
+		}
+		sc.close();
+	}
+}
